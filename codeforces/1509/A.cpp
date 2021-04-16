ffa2c9a1abdb85ddc6ef446ddaee0164908b01ba
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-04-16 20:25:00 +0530

    Add solution for problem `A - Average Height` Link: https://codeforces.com/contest/1509/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1509/submission/113203218

--- codeforces/1509/A.cpp
+++ codeforces/1509/A.cpp
@@ -1,151 +1,61 @@
-//**********************master._.mind**********************
-//-----------------Author: Akshat Aggarwal-----------------
-//If you don't understand the code below... dont worry, i didn't either
-#include "bits/stdc++.h"
+//*use cout<<"\n" instead of endl as endl is slower
+#include <bits/stdc++.h>
 using namespace std;
-using namespace std::chrono;
+typedef long long ll;
 
-#define ll long long
-#define db long double
-#define str string
-#define ull unsigned long long
-#define fo(i, n) for (ll i = 0; i < n; i++)
-#define ln '\n'
-#define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
-#define deb(x) cout << "[" << #x << "]: " << x << ln
-#define deb2(x, y) cout << "[" << #x << "]: " << x << ", [" << #y << "]: " << y << ln
-#define bit(x) __builtin_popcount(x)
-#define popb pop_back
-#define pb push_back
-#define eb emplace_back
-#define mp(x, y) make_pair(x, y)
-#define ub upper_bound
-#define lb lower_bound
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define uniq(x) (x).erase(unique(all(x)), (x).end())
-#define rall(x) (x).rbegin(), (x).rend()
-#define ps(x, y) fixed << setprecision(y) << x
-#define clr(x) memset(x, 0, sizeof(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
-#define sz(x) ((ll)(x).size())
-const ll mod = 1e9 + 7;  //1000000007
-const ll mod2 = 998244353;
-const ll inf = LLONG_MAX;
-const db eps = 1e-12;
-typedef pair<ll, ll> pl;
-typedef pair<db, db> pd;
-typedef vector<ll> vl;
-typedef vector<pl> vpl;
-typedef vector<vl> vvl;
-
-string to_string(string s) {
-    return '"' + s + '"';
+void swap(ll *a, ll *b)
+{
+    ll temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-string to_string(char ch) {
-    string s(1, ch);
-    return '\'' + s + '\'';
-}
+void segregateEvenOdd(ll arr[], ll size)
+{
+    ll left = 0, right = size - 1;
+    while (left < right)
+    {
+        /* Increment left index while we see 0 at left */
+        while (arr[left] % 2 == 0 && left < right)
+            left++;
 
-string to_string(const char *s) {
-    return to_string((string)s);
-}
+        /* Decrement right index while we see 1 at right */
+        while (arr[right] % 2 == 1 && left < right)
+            right--;
 
-string to_string(bool b) {
-    return (b ? "true" : "false");
-}
-
-template <typename A, typename B>
-string to_string(pair<A, B> p) {
-    return "{" + to_string(p.ff) + ", " + to_string(p.ss) + "}";
-}
-
-template <typename A>
-string to_string(A v) {
-    bool first = true;
-    string res = "{";
-    for (const auto &x : v) {
-        if (!first) {
-            res += ", ";
+        if (left < right)
+        {
+            /* Swap arr[left] and arr[right]*/
+            swap(&arr[left], &arr[right]);
+            left++;
+            right--;
         }
-        first = false;
-        res += to_string(x);
     }
-    res += "}";
-    return res;
 }
 
-void debug_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
-    cerr << to_string(H) << ln;
-    debug_out(T...);
-}
-
-#ifndef ONLINE_JUDGE
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-#else
-#define debug(...) 42
-#endif
-
-class Task {
-   public:
-    void Perform() {
-        Read();
-        Solve();
-    }
-
-   private:
+void solve()
+{
     ll n;
-    vl v;
+    cin >> n;
+    ll arr[n];
+    for (ll i = 0; i < n; i++)
+        cin >> arr[i];
+    segregateEvenOdd(arr, n);
 
-    void Read() {
-        cin >> n;
-        v.resize(n);
-        fo(i, n) cin >> v[i];
+    for (ll i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
     }
-
-    void Solve() {
-        vl e, o;
-        fo(i, n) {
-            if (v[i] & 1)
-                o.pb(v[i]);
-            else
-                e.pb(v[i]);
-        }
-
-        for (auto x : e) {
-            cout << x << " ";
-        }
-
-        for (auto x : o) {
-            cout << x << " ";
-        }
-        cout << "\n";
-    }
-};
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    ios_base::sync_with_stdio(false), cin.tie(nullptr);
-    auto start = chrono::high_resolution_clock::now();
-
-    ll tc;
-    cin >> tc;
-    while (tc--) {
-        Task t;
-        t.Perform();
-    }
-
-    auto end = chrono::high_resolution_clock::now();
-    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-    time_taken *= 1e-6;
-    cerr << ps(time_taken, 6) << " ms" << ln;
     return 0;
 }
