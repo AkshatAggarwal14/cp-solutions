f0cc8f3144399ae91c4e23afa79be604974fbae3
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-04-21 09:49:00 +0530

    Add solution for problem `A - Dubstep` Link: https://codeforces.com/contest/208/problem/A Tags: strings, *900 Ref: https://codeforces.com/contest/208/submission/113666456

--- codeforces/208/A.cpp
+++ codeforces/208/A.cpp
@@ -1,97 +1,86 @@
-/*	ॐ	*/
-// Akshat Aggarwal
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define fo(i, n) for (ll i = 0; i < n; i++)
-#define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define ps(x, y) fixed << setprecision(y) << x
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
-#define endl "\n"
-#define fast_io()                 \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0)
-const ll mod = 1e9 + 7;
-const ll inf = LLONG_MAX;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-
-void solve() {
-    string s;
-    cin >> s;
-    while (true) {
-        ll i = s.find("WUBWUB");
-        if (i == string::npos)
-            break;
-        s.replace(i, 3, "");
-    }
-    while (true) {
-        ll i = s.find("WUB");
-        if (i == string::npos)
-            break;
-        else if (i == 0)
-            s.replace(i, 3, "");
-        else
-            s.replace(i, 3, " ");
-    }
-    cout << s;
-
-    // string s;
-    // cin >> s;
-    // for (int i = 0; i < s.length(); i++)
-    // {
-    //     if (s[i] != 'W' || s[i + 1] != 'U' || s[i + 2] != 'B') //if it forms something else than WUB
-    //     {
-    //         cout << s[i];
-    //         if (s[i + 1] == 'W' && s[i + 2] == 'U' && s[i + 3] == 'B') // next 3 letters are WUB then space
-    //             cout << " ";
-    //     }
-    //     else
-    //         i += 2;
-    // }
-    /*
-    string s;
-    cin >> s;
-    while (true)
+#include <stdio.h>
+#include <string.h>
+int firstword(char *arr, int i);
+int secondword(char *arr, int i);
+int thirdword(char *arr, int i);
+int main()
+{
+    char arr[300];
+    int j = 0;
+    scanf("%s", arr);
+    int l = strlen(arr);
+    for (int i = 0; i < l; i++)
     {
-        ll i = s.find("WUB");
-        if (i == string::npos)
-            break;
-        else if (i == 0)
-            s.replace(i, 3, "");
-        else
-            s.replace(i, 3, " "); //!ADDS UNNECESSARY SPACES
-    }
-    cout << s;
-*/
-    return;
-}
-
-int main() {
-    fast_io();
-    ll t = 1;
-    //cin >> t;
-    while (t--) {
-        solve();
+        int m, n, p, u;
+        m = firstword(arr, i);
+        n = secondword(arr, i);
+        p = thirdword(arr, i);
+        if (m != 1 && n != 1 && p != 1)
+        {
+            char r = arr[i];
+            j++;
+            printf("%c", r);
+            u = firstword(arr, i + 1);
+            if (u == 1)
+            {
+                printf(" ");
+            }
+        }
     }
     return 0;
+}
+ 
+int firstword(char *arr, int n)
+{
+    if (arr[n] == 'W')
+    {
+        if (arr[n + 1] == 'U' && arr[n + 2] == 'B')
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+int secondword(char *arr, int n)
+{
+    if (arr[n] == 'U')
+    {
+        if (arr[n - 1] == 'W' && arr[n + 1] == 'B')
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+int thirdword(char *arr, int n)
+{
+    if (arr[n] == 'B')
+    {
+        if (arr[n - 1] == 'U' && arr[n - 2] == 'W')
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
 }
