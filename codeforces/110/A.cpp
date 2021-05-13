289993dd53e929dcd2a80e028ef17c9d96b5d0cd
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-05-13 16:39:00 +0530

    Add solution for problem `A - Nearly Lucky Number` Link: https://codeforces.com/contest/110/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/110/submission/116097016

--- codeforces/110/A.cpp
+++ codeforces/110/A.cpp
@@ -1,21 +1,26 @@
-#include<stdio.h>
+//*use cout<<"\n" instead of endl as endl is slower
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+typedef long long ll;
+
 int main()
 {
-    long long int n,x, count=0;
-    scanf("%lld", &n);
-    while(n)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t, c47 = 0, co = 0;
+    cin >> t;
+    while (t)
     {
-        x=n%10;
-        if(x==4 || x==7)
-           count++;
-        n/=10;
+        int dig = t % 10;
+        if (dig == 4 || dig == 7)
+            c47++;
+        t /= 10;
     }
- 
-    if(count==4 || count==7)
-        printf("YES");
+    if (c47 == 4 || c47 == 7)
+        cout << "YES\n";
     else
-        printf("NO");
- 
+        cout << "NO\n";
     return 0;
- 
 }
