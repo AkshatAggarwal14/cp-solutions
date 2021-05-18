727b4a0d73df3471f58dd48386a762d2dc32bb88
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-05-18 14:10:00 +0530

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/116546369

--- codeforces/1450/A.cpp
+++ codeforces/1450/A.cpp
@@ -5,32 +5,6 @@ using namespace std;
 #define rep(i, a, b) for (int i = a; i < b; i++)
 #define repeq(i, a, b) for (int i = a; i <= b; i++)
 #define repn(i, n) for (int i = 0; i < n; i++)
-const int MAX_CHAR = 26;
-
-// function to print string in sorted order
-void sortString(string &str)
-{
-    // Hash array to keep count of characters.
-    // Initially count of all charters is
-    // initialized to zero.
-    int charCount[MAX_CHAR] = {0};
-
-    // Traverse string and increment
-    // count of characters
-    for (int i = 0; i < str.length(); i++)
-
-        // 'a'-'a' will be 0, 'b'-'a' will be 1,
-        // so for location of character in count
-        // array we wil do str[i]-'a'.
-        charCount[str[i] - 'a']++;
-
-    // Traverse the hash array and print
-    // characters
-    for (int i = 0; i < MAX_CHAR; i++)
-        for (int j = 0; j < charCount[i]; j++)
-            cout << (char)('a' + i);
-    cout << "\n";
-}
 
 typedef long long ll;
 //SORT STRING TO SOLVE
@@ -40,10 +14,8 @@ void solve()
     cin >> n;
     string s;
     cin >> s;
-    // sort(s.begin(), s.end());
-    // cout << s << "\n";
-    // OR
-    sortString(s);
+    sort(s.begin(), s.end());
+    cout << s << "\n";
     return;
 }
 
