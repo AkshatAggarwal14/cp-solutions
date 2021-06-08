f2ba8ddf8882960639a212486367f48234256df6
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-06-08 09:33:00 +0530

    Add solution for problem `A - Sum of Round Numbers` Link: https://codeforces.com/contest/1352/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1352/submission/118767439

--- codeforces/1352/A.cpp
+++ codeforces/1352/A.cpp
@@ -40,34 +40,23 @@ typedef vector<vl> vvl;
 
 void solve()
 {
-    //! M1 number of round numbers required will be number of digits in n that are no zero, example: 509=500+9,1012=1000+10+2
-    string s;
-    cin >> s;
-    int n = s.length();
-    int count = 0;
-    fo(i, n) if (s[i] != '0') // count number of digits that are not zero
-        count++;
-    cout << count << endl;
-    for (int i = 0; i < n; ++i)
-        if (s[i] != '0')
-            cout << (s[i] - '0') * pow(10, (n - 1) - i) << " ";
 
-    //! M2 Brute force method by making a vector with all round numbers until n
-    // vl vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
-    // sort(all(vec), greater<ll>());
-    // vl ans;
-    // ll n, k = 0;
-    // cin >> n;
-    // for (ll i = 0; i < vec.size() && n > 0; i++)
-    // {
-    //     ll times = n / vec[i];
-    //     n -= vec[i] * times;
-    //     k += times;
-    //     if (times != 0)
-    //         ans.insert(ans.end(), times, vec[i]);
-    // }
-    // cout << k << "\n";
-    // tr(it, ans) cout << *it << " ";
+    vl vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+    sort(all(vec), greater<ll>());
+    vl ans;
+    ll n, k = 0;
+    cin >> n;
+    for (ll i = 0; i < vec.size() && n > 0; i++)
+    {
+        ll times = n / vec[i];
+        n -= vec[i] * times;
+        k += times;
+        if (times != 0)
+            ans.insert(ans.end(), times, vec[i]);
+        // fill_n(back_inserter(vec), times, vec[i]);
+    }
+    cout << k << "\n";
+    tr(it, ans) cout << *it << " ";
     return;
 }
 
