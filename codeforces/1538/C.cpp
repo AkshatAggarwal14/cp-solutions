0cc481f6c4e4e4edb109309ee0d2a9850c58f05a
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-06-10 23:04:00 +0530

    Add solution for problem `C - Number of Pairs` Link: https://codeforces.com/contest/1538/problem/C Tags: binary search, data structures, math, two pointers, *1300 Ref: https://codeforces.com/contest/1538/submission/119082594

--- codeforces/1538/C.cpp
+++ codeforces/1538/C.cpp
@@ -1,61 +1,53 @@
-#ifdef LOCAL
-// https://github.com/AkshatAggarwal14/Competetive-Programming
-#include "Akshat.hpp"
-#else
-#include "bits/stdc++.h"
-using namespace std;
-#define dbg(...)
-#endif
-using ll = int64_t;
-auto sz = [](const auto &container) -> ll { return container.size(); };
-#define all(x) (x).begin(), (x).end()
-template <class T, class U = T>
-bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
-template <class T, class U = T>
-bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
-
-template <bool b>
-auto binsearch(auto l, auto r, const auto &pred) {
-    --l, ++r;
-    for (decltype(l) m; m = midpoint(l, r), r > l + 1;) (pred(m) ? l : r) = m;
-    return (b ? l : r);
-}
-
-// returns first i in [l, r], p(i) false, and if none found, returns r + 1
-auto find_first_false(auto l, auto r, const auto &p) {
-    return binsearch<false>(l, r, p);
-}
-
-// returns last i in [l, r], p(i) true, and if none found, returns l - 1
-auto find_last_true(auto l, auto r, const auto &p) {
-    return binsearch<true>(l, r, p);
-}
-
-void Solution() {
-    ll n, l, r;
-    cin >> n >> l >> r;
-    vector<ll> a(n);
-    for (ll &x : a) cin >> x;
-    sort(all(a));
-    // i < j, !(a[i] + a[j] < l), !(a[i] + a[j] > r)
-    ll ans = 0;
-    for (ll i = 0; i < n; ++i) {
-        ll aJ_start = find_first_false(i + 1, n - 1, [&](ll idx) { return a[i] + a[idx] < l; });
-        ll aJ_end = find_last_true(i + 1, n - 1, [&](ll idx) { return a[i] + a[idx] <= r; });
-        if (aJ_end >= aJ_start) ans += (aJ_end - aJ_start + 1);
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    #include <set>
+     
+    long long countPair(long long arr[], int size, long long a, long long b) {
+        long long cnt = 0;
+        long long i = 0, low = size-1, high = size-1;
+        while (i < high) {
+            //find the lower bound such that arr[i] + arr[low] < a, 
+            //meanwhile arr[i]+arr[low+1] >= a
+             low = std::max(i, low);
+             while (low > i && arr[i] + arr[low] >= a) --low;
+     
+            //find an upper bound such that arr[i] + arr[high] <= b 
+            //meanwhile, arr[i]+arr[high+1] > b
+            while (high > low && arr[i] + arr[high] > b) --high; 
+            //all pairs: arr[i]+arr[low+1], arr[i]+arr[low+2],...,arr[i]+arr[high]
+            //are in the rage[a, b], and we count it as follows.
+            cnt += (high-low);
+            ++i;
+        }
+        return cnt;
     }
-    cout << ans << '\n';
-}
-
-// clang-format off
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    cout << fixed << setprecision(12);
-    ll tc; cin >> tc; while (tc--)
-    Solution();
-    return 0;
-}
+     
+    int main(){
+    	std::ios::sync_with_stdio(false);
+    	std::cin.tie(NULL);
+    	
+    	int testcases;
+    	std::cin >> testcases;
+    	
+    	for(int test=0; test<testcases; test++){
+    		long long n;
+    		long long l,r;
+    		std::cin >> n >> l >> r;
+     
+    		long long a[n];
+     
+    		for (long long i = 0; i < n; ++i){
+    			std::cin >> a[i];
+    		}
+    		// n = removeDuplicates(a,n);
+    		std::sort(a,a+n);
+    		long long answer = countPair(a,n,l,r);
+     
+    		std::cout << answer << "\n";
+    		
+    	}//testcases
+    		
+    	return 0;
+    }//main
+    // 1 2 3 4 5
