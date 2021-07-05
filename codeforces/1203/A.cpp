043cbf868e041bccafd4fc57c246e1260d0b86be
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-07-05 20:25:00 +0530

    Add solution for problem `A - Circle of Students` Link: https://codeforces.com/contest/1203/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/1203/submission/121406217

--- codeforces/1203/A.cpp
+++ codeforces/1203/A.cpp
@@ -1,83 +1,107 @@
-//@aryanpathania
+//****************master._.mind****************
+//-----------Author: Akshat Aggarwal-----------
 #include <bits/stdc++.h>
-#define MOD 1'000'000'007
-//#define MOD = 998'244'353
-#define int int64_t
-typedef long long ll;
-typedef long double ld;
 using namespace std;
 
-bool compare(int a, int b) {
-    return a > b;
+#define ll long long
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+#define ub() upper_bound()
+#define lb() lower_bound()
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define ps(x, y) fixed << setprecision(y) << x
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define endl "\n"
+#define fast_io()                 \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+const ll mod = 1e9 + 7;
+const ll inf = LLONG_MAX;
+const double eps = 1e-12;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+bool checkClockwise(ll arr[], ll n)
+{
+    ll element = INT_MAX; // min element
+    ll index = -1;        // min index
+
+    for (ll i = 0; i < n; i++)
+    {
+        if (arr[i] < element)
+        {
+            element = arr[i];
+            index = i;
+        }
+    }
+
+    ll flag1 = 1;
+    for (ll i = 1; i < index; i++)
+    {
+        if (arr[i] < arr[i - 1]) // all elements before minimum index must be in increasing order
+        {
+            flag1 = 0;
+            break;
+        }
+    }
+
+    ll flag2 = 1;
+    for (ll i = index + 1; i < n; i++)
+    {
+        if (arr[i] < arr[i - 1]) // all elements after minimum index must be in increasing order
+        {
+            flag2 = 0;
+            break;
+        }
+    }
+
+    if (flag1 && flag2 && (arr[0] == arr[n - 1] + 1 || (arr[0] == 1 && arr[n - 1] == n)))
+        return true;
+    return false;
 }
 
-void solve() {
-    int n;
-    std::cin >> n;
-    std::deque<int> d(n);
-    for (int i = 0; i < n; i++) {
-        std::cin >> d[i];
-    }
+void solve()
+{
 
-    while (d[0] != 1) {
-        int temp = d[0];
-        d.pop_front();
-        d.push_back(temp);
-    }
+    ll n;
+    cin >> n;
+    ll arr[n], rev[n];
+    fo(i, n) cin >> arr[i];
+    fo(i, n) rev[n - i - 1] = arr[i];
 
-    if (std::is_sorted(d.begin(), d.end())) {
-        std::cout << "YES" << '\n';
-        return;
-    }
-
-    int temp = d[0];
-    d.pop_front();
-    d.push_back(temp);
-
-    // if (std::is_sorted(d.begin(), d.end(), greater<int>())) { //!SAME
-    if (std::is_sorted(d.begin(), d.end(), compare)) {
-        std::cout << "YES" << '\n';
-        return;
-    }
-
-    std::cout << "NO" << '\n';
+    if (checkClockwise(arr, n) || checkClockwise(rev, n))
+        cout << "YES";
+    else
+        cout << "NO";
     return;
 }
 
-int32_t main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-
-    int testcases;
-    std::cin >> testcases;
-
-    while (testcases--) {
+int main()
+{
+    fast_io();
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
         solve();
+        cout << "\n";
     }
-
     return 0;
 }
-/*
-───────▄▀▀▀▀▀▀▀▀▀▀▄▄
-────▄▀▀░░░░░░░░░░░░░▀▄
-──▄▀░░░░░░░░░░░░░░░░░░▀▄
-──█░░░░░░░░░░░░░░░░░░░░░▀▄
-─▐▌░░░░░░░░▄▄▄▄▄▄▄░░░░░░░▐▌
-─█░░░░░░░░░░░▄▄▄▄░░▀▀▀▀▀░░█
-▐▌░░░░░░░▀▀▀▀░░░░░▀▀▀▀▀░░░▐▌
-█░░░░░░░░░▄▄▀▀▀▀▀░░░░▀▀▀▀▄░█
-█░░░░░░░░░░░░░░░░▀░░░▐░░░░░▐▌
-▐▌░░░░░░░░░▐▀▀██▄░░░░░░▄▄▄░▐▌
-─█░░░░░░░░░░░▀▀▀░░░░░░▀▀██░░█
-─▐▌░░░░▄░░░░░░░░░░░░░▌░░░░░░█
-──▐▌░░▐░░░░░░░░░░░░░░▀▄░░░░░█
-───█░░░▌░░░░░░░░▐▀░░░░▄▀░░░▐▌
-───▐▌░░▀▄░░░░░░░░▀░▀░▀▀░░░▄▀
-───▐▌░░▐▀▄░░░░░░░░░░░░░░░░█
-───▐▌░░░▌░▀▄░░░░▀▀▀▀▀▀░░░█
-───█░░░▀░░░░▀▄░░░░░░░░░░▄▀
-──▐▌░░░░░░░░░░▀▄░░░░░░▄▀
-─▄▀░░░▄▀░░░░░░░░▀▀▀▀█▀
-▀░░░▄▀░░░░░░░░░░▀░░░▀▀▀▀▄▄▄▄▄
-
-*/
