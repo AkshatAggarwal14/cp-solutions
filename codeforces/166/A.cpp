048c03d830fb19a07e92e18cf7cfa5895ca77c77
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-07-14 09:15:00 +0530

    Add solution for problem `A - Rank List` Link: https://codeforces.com/contest/166/problem/A Tags: binary search, implementation, sortings, *1100 Ref: https://codeforces.com/contest/166/submission/122399674

--- codeforces/166/A.cpp
+++ codeforces/166/A.cpp
@@ -1,201 +1,104 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define lld long double
-#define ull unsigned long long
-#define fo(i, n) for (ll i = 0; i < n; ++i)
-#define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp(x, y) make_pair(x, y)
-#define ub upper_bound
-#define lb lower_bound
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define uniq(x) (x).erase(unique(all(x)), (x).end())
-#define rall(x) (x).rbegin(), (x).rend()
-#define ps(x, y) fixed << setprecision(y) << x
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
-#define sz(x) ((ll)(x).size())
-#define ln '\n'
-#define cy cout << "YES"
-#define cno cout << "NO"
-#define cn cout << '\n'
-#define re return
-#define fast_io()                 \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0)
-//cin.tie(NULL) is used to read all input before displaying any output
-const ll mod = 1e9 + 7;  //1000000007
-const ll mod2 = 998244353;
-const ll inf = LLONG_MAX;
-const double eps = 1e-12;
-typedef pair<ll, ll> pl;
-typedef pair<double, double> pd;
-typedef vector<ll> vl;
-typedef vector<pl> vpl;
-typedef vector<vl> vvl;
-
-//* --------Debug--------
-void _print(ll t) { cerr << t; }
-void _print(int t) { cerr << t; }
-void _print(string t) { cerr << t; }
-void _print(char t) { cerr << t; }
-void _print(lld t) { cerr << t; }
-void _print(double t) { cerr << t; }
-void _print(ull t) { cerr << t; }
-template <class T, class V>
-void _print(pair<T, V> p);
-template <class T>
-void _print(vector<T> v);
-template <class T>
-void _print(set<T> v);
-template <class T, class V>
-void _print(map<T, V> v);
-template <class T>
-void _print(multiset<T> v);
-
-//divisor_count(ll)
-//prime_facorisation(n)
-//primes_less_than(n)
-
-//* Global vars
-ll n, k;
-
-inline bool cmp(const pair<ll, ll> &a, const pair<ll, ll> &b) {
-    if (a.ff > b.ff)
-        return true;
-    else if (a.ff == b.ff) {
-        if (a.ss < b.ss)
-            return true;
-        else
-            return false;
-    } else
-        return false;
+int same_place(int *a, int p, int s, int e)
+{
+    int temp = 0;
+    for (int i = s; i <= e; i++)
+    {
+        if (a[i] == p)
+        {
+            temp++;
+        }
+    }
+    return temp;
 }
 
-void solve() {
+void custom_sort(int *a, int *b, int n)
+{
+    int flag;
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        flag = 1;
+        for (int j = n - 1; j > i; j--)
+        {
+            if (a[j] > a[j - 1])
+            {
+                flag = 0;
+                temp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = temp;
+                temp = b[j];
+                b[j] = b[j - 1];
+                b[j - 1] = temp;
+            }
+        }
 
+        if (flag)
+        {
+            return;
+        }
+    }
+}
+
+int starting_index(int *a, int q, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == q)
+        {
+            return i;
+        }
+    }
+    return n-1;
+}
+int ending_index(int *a, int starting, int value, int n)
+{
+    for (int i = starting; i < n; i++)
+    {
+        if (a[i] != value)
+        {
+            return i-1;
+        }
+    }
+    return n-1 ;
+}
+int finding_penalty(int *a, int s, int e, int k)
+{
+    if (s==e)
+    {
+        return a[s];
+    }
+    
+    sort(a + s, a + (e+1));
+    return a[k - 1];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, k;
+    int questions, penalty;
+    int starting, ending;
     cin >> n >> k;
-    //!m1
-    /*
-    vector<pair<ll, ll>> arr(n);
-    map<pair<ll, ll>, ll> mep;
-    fo(i, n) {
-        cin >> arr[i].ff >> arr[i].ss;
-        mep[arr[i]]++;
+    int *question_solve = new int[n];
+    int *penalty_time = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> question_solve[i] >> penalty_time[i];
     }
-    sort(all(arr), cmp);
-    _print(arr);
-    cout << mep[arr[k - 1]] << '\n';
-    */
-
-    //!m2
-    /* 
-    fo (i, n) arr[i].ss = - arr[i].ss; //!<-- for sorting in desc order
-    sort(all(arr), greater<>());
-    int x, y, ans = 0;
-    x = arr[k-1].ff; y = arr[k-1].ss;
-    fo (i, n) if (arr[i].ff == x and arr[i].ss == y) ans++;
-    cout << ans;
-    */
-
-    //!m3
-    vpl arr(n);
-    map<pl, ll> mep;
-    fo(i, n) {
-        cin >> arr[i].ff >> arr[i].ss;
-        mep[arr[i]]++;
-    }
-    sort(all(arr), [&](pl a, pl b) {
-        if (a.ff > b.ff)
-            return true;
-        else if (a.ff == b.ff) {
-            if (a.ss < b.ss)
-                return true;
-            else
-                return false;
-        } else
-            return false;
-    });
-    _print(arr);
-    cout << mep[arr[k - 1]] << '\n';
-
-    //**********************CODE ENDS HERE**********************
-    cn;
-    return;
-}  //todo solve
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
-#endif
-    fast_io();
-    clock_t start, end;
-    start = clock();
-
-    ll tc = 1;
-    //cin >> tc;
-    while (tc--)
-        solve();
-
-    end = clock();
-    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-    cerr << "Time taken:" << ln << ps(time_taken, 6) << " sec\n";
+    custom_sort(question_solve, penalty_time, n); //checked
+    questions = question_solve[k - 1];            //checked
+    starting = starting_index(question_solve, questions, n);
+    ending = ending_index(question_solve, starting, questions, n);
+    penalty = finding_penalty(penalty_time, starting, ending, k);
+    cout << same_place(penalty_time, penalty, starting, ending) << endl;
+    delete [] question_solve;
+    delete [] penalty_time;
     return 0;
-}  //todo main
-
-//* ------Debug------
-template <class T, class V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <class T>
-void _print(vector<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(set<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T>
-void _print(multiset<T> v) {
-    cerr << "[ ";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <class T, class V>
-void _print(map<T, V> v) {
-    cerr << "[ ";
-    for (auto i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+    //7 2 4 10 4 10 4 10 3 20 2 1 2 1 1 10
+    //5 4 3 1 3 1 5 3 3 1 3 1
 }
