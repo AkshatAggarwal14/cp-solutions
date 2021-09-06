ac2bb3136113034d429689d7f58c07e1ab88db07
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-06 18:55:00 +0530

    Add solution for problem `B - T-shirt buying` Link: https://codeforces.com/contest/799/problem/B Tags: data structures, implementation, *1400 Ref: https://codeforces.com/contest/799/submission/128048523

--- codeforces/799/B.cpp
+++ codeforces/799/B.cpp
@@ -1,95 +1,18 @@
-#ifndef ONLINE_JUDGE
-#include "Akshat.hpp"
-#else
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
 #include "bits/stdc++.h"
 using namespace std;
-#define dbg(...)
-#define debug(...)
-#endif
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T>
-using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//member functions :
-//1. order_of_key(k) : number of elements strictly lesser than k
-//2. find_by_order(k) : k-th element in the set
 
 using ll = int64_t;
 using db = double;
 using str = string;
 using ull = unsigned long long;
 #define fo(i, n) for (int i = 0; i < n; i++)
-#define ln '\n'
-#define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
-#define deb(x) cout << "[" << #x << "]: " << x << ln
-#define deb2(x, y) cout << "[" << #x << "]: " << x << ", [" << #y << "]: " << y << ln
-#define bit(x) __builtin_popcount(x)
-#define bitll(x) __builtin_popcountll(x)
-#define pb push_back
-#define eb emplace_back
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define uniq(x) (x).erase(unique(all(x)), (x).end())
-#define rall(x) (x).rbegin(), (x).rend()
-#define ps(x, y) fixed << setprecision(y) << x
-#define clr(x) memset(x, 0, sizeof(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
 #define sz(x) ((ll)(x).size())
-#define present(b, a) ((a).find((b)) != (a).end())
-#define yes() cout << "YES\n"
-#define no() cout << "NO\n"
-const ll mod = 1e9 + 7;  //1000000007
-const ll mod2 = 998244353;
-const ll inf = LLONG_MAX;
-const db eps = 1e-12;
-typedef pair<ll, ll> pl;
 typedef pair<int, int> pi;
-typedef pair<db, db> pd;
-typedef vector<ll> vl;
-typedef vector<pl> vpl;
-typedef vector<vl> vvl;
 typedef vector<int> vi;
 typedef vector<pi> vpi;
-typedef vector<vi> vvi;
-
-template <typename T, typename T1>
-T amax(T &a, T1 b) {
-    if (b > a) a = b;
-    return a;
-}
-template <typename T, typename T1>
-T amin(T &a, T1 b) {
-    if (b < a) a = b;
-    return a;
-}
-
-//*Operator overloads
-template <typename T1, typename T2>  // cin >> pair<T1, T2>
-istream &operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); }
-template <typename T>  // cin >> vector<T>
-istream &operator>>(istream &istream, vector<T> &v) {
-    for (auto &it : v) cin >> it;
-    return istream;
-}
-template <typename T1, typename T2>  // cout << pair<T1, T2>
-ostream &operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
-template <typename T>  // cout << vector<T>
-ostream &operator<<(ostream &ostream, const vector<T> &c) {
-    for (auto &it : c) cout << it << " ";
-    return ostream;
-}
-
-template <typename T>
-void print(T &&t) { cout << t << "\n"; }
-template <typename T, typename... Args>
-void print(T &&t, Args &&...args) {
-    cout << t << " ";
-    print(forward<Args>(args)...);
-}
 
 void Solution() {
     int n;
@@ -109,6 +32,7 @@ void Solution() {
         cin >> temp;
         pi P = {-1, -1};
         int mini = INT_MAX;
+        //! these are only possibilities
         vpi T = {{1, temp}, {2, temp}, {3, temp}, {temp, 1}, {temp, 2}, {temp, 3}};
         for (pi pr : T) {
             if (S.count(pr)) {
@@ -129,7 +53,6 @@ void Solution() {
         }
     }
 }
-//*read stuff at the bottom
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -137,13 +60,7 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
     ios_base::sync_with_stdio(false), cin.tie(nullptr);
-
-    ll tc = 1;
-    //cin >> tc;
-    while (tc--) {
-        Solution();
-    }
-
+    Solution();
     cerr << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
     return 0;
 }
