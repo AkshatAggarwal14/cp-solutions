2092d9ba9abd893042840c9e93a8cb94fe04c725
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-13 12:20:00 +0530

    Add solution for problem `D1 - Seating Arrangements (easy version) ` Link: https://codeforces.com/contest/1566/problem/D1 Tags: data structures, greedy, sortings, *1100 Ref: https://codeforces.com/contest/1566/submission/128701037

--- codeforces/1566/D1.cpp
+++ codeforces/1566/D1.cpp
@@ -7,37 +7,29 @@ using namespace std;
 #define debug(...)
 #endif
 
-// ----------------------------<optimizations>----------------------------
-/*
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-*/
-// ---------------------------</optimizations>----------------------------
-
-// ---------------------------------<PBDS>--------------------------------
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template <class T>
-using o_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-template <class T>
-using o_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-template <class key, class value, class cmp = std::less<key>>
-using o_map = __gnu_pbds::tree<key, value, cmp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+template <typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 //member functions :
-// 1. order_of_key (val): returns the no. of values less than val
-// 2. find_by_order (k): returns the iterator to kth largest element.(0-based)
-// for ordered_multiset, lower_bound and upper_bound swap roles
-// --------------------------------</PBDS>--------------------------------
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
 
-// -------------------------<Defines and typedefs>------------------------
 using ll = int64_t;
 using db = double;
 using str = string;
 using ull = unsigned long long;
 #define fo(i, n) for (ll i = 0; i < n; i++)
+#define ln '\n'
 #define rep(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? i++ : i--)
-#define set_bits(x) __builtin_popcountll(x)
+#define deb(x) cout << "[" << #x << "]: " << x << ln
+#define deb2(x, y) cout << "[" << #x << "]: " << x << ", [" << #y << "]: " << y << ln
+#define bit(x) __builtin_popcount(x)
+#define bitll(x) __builtin_popcountll(x)
+#define pb push_back
 #define eb emplace_back
 #define ff first
 #define ss second
@@ -45,70 +37,93 @@ using ull = unsigned long long;
 #define uniq(x) (x).erase(unique(all(x)), (x).end())
 #define rall(x) (x).rbegin(), (x).rend()
 #define ps(x, y) fixed << setprecision(y) << x
+#define clr(x) memset(x, 0, sizeof(x))
 #define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
 #define sz(x) ((ll)(x).size())
-template <class T>
-using V = vector<T>;
-using vl = V<ll>;
-using vvl = V<vl>;
-template <class T, class U>
-using P = pair<T, U>;
-using pl = P<ll, ll>;
-using vpl = V<pl>;
-// ------------------------</Defines and typedefs>------------------------
-
-// --------------------------------<Consts>-------------------------------
+#define present(b, a) ((a).find((b)) != (a).end())
+#define yes() cout << "YES\n"
+#define no() cout << "NO\n"
 const ll mod = 1e9 + 7;  //1000000007
 const ll mod2 = 998244353;
 const ll inf = LLONG_MAX;
 const db eps = 1e-12;
-const double PI = acos(-1);
-// -------------------------------</Consts>-------------------------------
+typedef pair<ll, ll> pl;
+typedef pair<int, int> pi;
+typedef pair<db, db> pd;
+typedef vector<ll> vl;
+typedef vector<pl> vpl;
+typedef vector<vl> vvl;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
 
-// -------------------------------<Templates>-----------------------------
-template <class T, class U = T>
-bool amin(T& a, U&& b) {
-    return b < a ? a = std::forward<U>(b), true : false;
+template <typename T, typename T1>
+T amax(T &a, T1 b) {
+    if (b > a) a = b;
+    return a;
 }
-template <class T, class U = T>
-bool amax(T& a, U&& b) {
-    return a < b ? a = std::forward<U>(b), true : false;
+template <typename T, typename T1>
+T amin(T &a, T1 b) {
+    if (b < a) a = b;
+    return a;
 }
-// ------------------------------</Templates>-----------------------------
 
-// ---------------------------------<Solve>-------------------------------
+//*Operator overloads
+template <typename T1, typename T2>  // cin >> pair<T1, T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); }
+template <typename T>  // cin >> vector<T>
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v) cin >> it;
+    return istream;
+}
+template <typename T1, typename T2>  // cout << pair<T1, T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
+template <typename T>  // cout << vector<T>
+ostream &operator<<(ostream &ostream, const vector<T> &c) {
+    for (auto &it : c) cout << it << " ";
+    return ostream;
+}
+
+template <typename T>
+void print(T &&t) { cout << t << "\n"; }
+template <typename T, typename... Args>
+void print(T &&t, Args &&...args) {
+    cout << t << " ";
+    print(forward<Args>(args)...);
+}
 
 void Solution() {
-    int n, m;
+    ll n, m;
     cin >> n >> m;
-    vector<int> v(n * m);
-    for (auto& x : v) cin >> x;
-
-    vector<int> s(v);
-    sort(all(s));
-    vector<int> pos(n * m);
-    vector<vector<int>> sit(n, vector<int>(m, -1));  //initially all -1
-    for (int i = 0; i < n * m; ++i) {
-        pos[i] = i;
+    // here n=1
+    vl a(m);
+    cin >> a;
+    o_multiset<ll> st;
+    ll ans = 0;
+    fo(i, m) {
+        st.insert(a[i]);
+        ll inc = st.order_of_key(a[i]);
+        ans += inc;
     }
-    debug(s);
-    debug(pos);
-    int ans = 0;
-    for (int i = 0; i < n * m; i++) {
-        int l = lower_bound(all(s), v[i]) - s.begin();
-        int val = pos[l];
-        int row = val / m;
-        int col = val % m;
-        for (int j = 0; j < col; j++) {
-            if (sit[row][j] != -1 && sit[row][j] != v[i]) ans++;
-        }
-        sit[row][col] = v[i];
-        pos[l]++;
-    }
-    cout << ans << "\n";
+    print(ans);
 }
 
-// --------------------------------</Solve>-------------------------------
+void solve() {
+    ll n, m;
+    cin >> n >> m;
+    // here n=1
+    vl a(m);
+    dbg(a);
+    ll ans = 0;
+    fo(i, m) {
+        cin >> a[i];
+        fo(j, i) {
+            ans += (a[j] < a[i]);
+        }
+    }
+    print(ans);
+}
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -116,14 +131,14 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
     cin.tie(nullptr)->sync_with_stdio(false);
+
     ll tc = 1;
     cin >> tc;
     while (tc--) {
         Solution();
     }
-#ifndef ONLINE_JUDGE
-    cerr << fixed << setprecision(4) << (double)clock() / CLOCKS_PER_SEC << " secs" << endl;
-#endif
+
+    cerr << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
     return 0;
 }
 
@@ -133,12 +148,12 @@ int main() {
     * int overflows, array bounds, etc.
     * special cases (n=1)?
     * do something instead of nothing
-    * n % mod = (n % mod + mod) % mod; (Handling negatives)
+    * modulo of negative numbers is not a%b, it is a%b + abs(b)
     * When using a set, lower_bound(all(set),l) is slower than set.lower_bound(l) because of random iterators
     * string .append() or += is O1, but s = s + s is On (as it creates a copy first), use wisely
     * DONT GET STUCK ON ONE APPROACH
     * use __lg(n) instead of log2(n), int: 32 - __builtin_clz(n), ll: 63 - __builtin_clzll(n), https://codeforces.com/blog/entry/45966
     * string.rfind() finds first occurence from end
     * (a & b) + (a | b) = a + b
-    * TLE due to ll ? or multiple copies of large DS?
+    * TLE due to ll ?
 */
