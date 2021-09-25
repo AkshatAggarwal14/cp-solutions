4fbd7f60c0ea5fbb0ebba036b382584bfec21469
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-25 22:04:00 +0530

    Add solution for problem `C - Bertown Subway` Link: https://codeforces.com/contest/884/problem/C Tags: dfs and similar, greedy, math, *1500 Ref: https://codeforces.com/contest/884/submission/129869862

--- codeforces/884/C.cpp
+++ codeforces/884/C.cpp
@@ -1,40 +1,62 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
 using ll = int64_t;
-
-const ll N = 1e5;
-vector<int> adj[N];
-bool vis[N];
-ll len = 0;
-
-void dfs(ll x) {
-    vis[x] = true;
-    len++;
-    for (auto node : adj[x]) {
-        if (vis[node]) continue;
-        dfs(node);
-    }
-}
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define sz(x) ((ll)(x).size())
+const double eps = 1e-12;
+const double PI = acos(-1);
 
 void Solution() {
-    vector<ll> lens;
-    ll n, num;
+    /*
+    https://codeforces.com/contest/884/problem/C
+        1. pi = j and only one, so one one relation ship
+        2. We just have to connect two different loops and make it into larger one
+        3. Loop:
+            1 -> 2, 2-> 3, 3-> 1 (Here we can have 12,23,31,13,32,21,11,22,33) -> n^2, where n is size of a loop
+        4. to maximise we need (a+b)^2 - a^2 - b^2 = 2*a*b <- product to be maximum <- basically 2 with maximum size of loops
+    */
+    //!m1 - graph
+
+    //!m2
+    ll n;
     cin >> n;
-    for (ll i = 1; i <= n; ++i) cin >> num, adj[i].push_back(num);
-    for (ll i = 1; i <= n; ++i) {
-        if (vis[i]) continue;
-        len = 0;
-        dfs(i);
-        if (len) lens.push_back(len);
+    vector<ll> p(n);
+    fo(i, n) {
+        cin >> p[i];
+        --p[i];  // i
     }
-    sort(lens.begin(), lens.end());
-    ll ans;
-    if ((ll)lens.size() > 1) {
-        ans = static_cast<ll>(pow((lens[(ll)lens.size() - 2] + lens.back()), 2));
-        for (ll i = 0; i < (ll)lens.size() - 2; i++) ans += lens[i] * lens[i];
-    } else
-        ans = lens[0] * lens[0];
-    cout << ans << '\n';
+    vector<ll> loops;  //length of all loops
+    vector<bool> vis(n, false);
+    fo(i, n) {
+        if (!vis[i]) {
+            ll j = i;
+            ll len = 1;
+            vis[i] = true;
+            while (p[j] != i) {    //go to next one until reach 1st again
+                vis[p[j]] = true;  //mark it as visited
+                j = p[j];
+                len++;
+            }
+            loops.push_back(len);
+        }
+    }
+    ll N = sz(loops);
+    if (N == 1) {
+        cout << loops[0] * loops[0];
+    } else {
+        sort(loops.begin(), loops.end());
+        ll ans = (loops[N - 2] + loops[N - 1]) * (loops[N - 2] + loops[N - 1]);
+        fo(i, sz(loops) - 2) ans += loops[i] * loops[i];
+        cout << ans;
+    }
+    cout << '\n';
 }
 
 int main() {
@@ -43,6 +65,14 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    Solution();
+#ifdef NCR
+    init();
+#endif
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        Solution();
+    }
     return 0;
 }
