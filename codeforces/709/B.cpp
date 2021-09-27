4c4006d0fe00238473a0cb6f83ce78d9b64cf3b0
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-09-27 15:31:00 +0530

    Add solution for problem `B - Checkpoints` Link: https://codeforces.com/contest/709/problem/B Tags: greedy, implementation, sortings, *1500 Ref: https://codeforces.com/contest/709/submission/129987755

--- codeforces/709/B.cpp
+++ codeforces/709/B.cpp
@@ -1,24 +1,38 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
 using ll = int64_t;
+#define fo(i, n) for (ll i = 0; i < n; i++)
+#define sz(x) ((ll)(x).size())
+const double eps = 1e-12;
+const double PI = acos(-1);
 
 void Solution() {
     ll n, a;
     cin >> n >> a;
     vector<ll> x(n);
-    for (ll i = 0; i < n; ++i) cin >> x[i];
+    fo(i, n) cin >> x[i];
     sort(x.begin(), x.end());
-    if (n == 1) return void(cout << 0 << '\n');  // n-1 = 0, so no steps needed
+    if (n == 1) {
+        cout << 0 << '\n';
+        return;
+    }
     ll ans = LLONG_MAX;
-    // Excluding 1 num, gives us only 3 choices for maximum and minimum
     vector<pair<ll, ll>> V = {{x[0], x[n - 1]}, {x[0], x[n - 2]}, {x[1], x[n - 1]}};
     for (auto [x, y] : V) {
         ll mn = x, mx = y;
         if (a >= mn && a <= mx) {
-            ans = min(ans, (mx - mn) + min(mx - a, a - mn));  // overlap when in between
+            ans = min(ans, (mx - mn) + min(mx - a, a - mn));
         } else {
-            set<ll> st = {mn, mx, a};
-            ans = min(ans, *st.rbegin() - *st.begin());
+            vector<ll> vv = {mn, mx, a};
+            sort(vv.begin(), vv.end());
+            ans = min(ans, vv[2] - vv[0]);
         }
     }
     cout << ans << '\n';
@@ -26,6 +40,18 @@ void Solution() {
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    Solution();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#ifdef NCR
+    init();
+#endif
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        Solution();
+    }
     return 0;
 }
