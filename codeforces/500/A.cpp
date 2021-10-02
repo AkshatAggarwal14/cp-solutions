2d024c77b6f1709ff9e28dace85b05cc9ee0394c
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-02 15:04:00 +0530

    Add solution for problem `A - New Year Transportation` Link: https://codeforces.com/contest/500/problem/A Tags: dfs and similar, graphs, implementation, *1000 Ref: https://codeforces.com/contest/500/submission/130546655

--- codeforces/500/A.cpp
+++ codeforces/500/A.cpp
@@ -1,22 +1,34 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#define debug(...)
+#endif
+
 using ll = int64_t;
+#define fo(i, n) for (ll i = 0; i < n; i++)
 #define sz(x) ((ll)(x).size())
+const double eps = 1e-12;
+const double PI = acos(-1);
 
 void Solution() {
     ll n, t, num;
     cin >> n >> t;
-    vector<vector<ll>> adj(n + 1);
+    vector<vector<int>> adj(n + 1);
     vector<bool> vis(n + 1, false);
-    for (ll i = 1; i <= n - 1; ++i) {
+    fo(i, n - 1) {
         cin >> num;
-        adj[i].push_back(i + num);
+        adj[i + 1].push_back(i + num + 1);
     }
+
     function<void(ll)> dfs = [&](ll parent) {
         vis[parent] = true;
         for (auto child : adj[parent])
             if (!vis[child]) dfs(child);
     };
+
     dfs(1);
     if (vis[t])
         cout << "YES";
@@ -30,6 +42,14 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    Solution();
+#ifdef NCR
+    init();
+#endif
+    ll tc = 1;
+    //cin >> tc;
+    for (ll i = 1; i <= tc; ++i) {
+        // cout << "Case #" << i << ": ";
+        Solution();
+    }
     return 0;
 }
