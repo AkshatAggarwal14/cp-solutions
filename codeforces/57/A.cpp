21ba630d4552ec162990984c879e68d114a3f79e
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-06 22:48:00 +0530

    Add solution for problem `A - Square Earth?` Link: https://codeforces.com/contest/57/problem/A Tags: dfs and similar, greedy, implementation, *1300 Ref: https://codeforces.com/contest/57/submission/131001346

--- codeforces/57/A.cpp
+++ codeforces/57/A.cpp
@@ -4,10 +4,11 @@ using namespace std;
 using ll = int64_t;
 
 ll n;
+vector<vector<bool>> vis;
 vector<vector<ll>> dis;
 bool valid(ll x, ll y) {
     if (x < 0 || y < 0 || x > n || y > n) return false;
-    if (dis[x][y] != 1e9) return false;  // if already visited or empty
+    if (vis[x][y]) return false;  // if already visited or empty
     return true;
 }
 
@@ -18,21 +19,22 @@ ll dy[] = {0, 1, 0, -1};
 void Solution() {
     ll x1, y1, x2, y2;
     cin >> n >> x1 >> y1 >> x2 >> y2;
-    dis.resize(n + 1, vector<ll>(n + 1, 1e9));  // distance + visited
+    dis.resize(n + 1, vector<ll>(n + 1, 0));
     queue<pair<ll, ll>> bfs;
+    vis.resize(n + 1, vector<bool>(n + 1, false));
     bfs.emplace(x1, y1);
-    dis[x1][y1] = 0;
-    ll x, y, X, Y;
+    vis[x1][y1] = true;
     while (!bfs.empty()) {
-        x = bfs.front().first;
-        y = bfs.front().second;
+        ll x = bfs.front().first;
+        ll y = bfs.front().second;
         bfs.pop();
         if (x == x2 && y == y2) break;
         if (x == 0 || x == n || y == 0 || y == n) {
             for (ll i = 0; i < 4; ++i) {  // 4 directions
-                X = x + dx[i];
-                Y = y + dy[i];
+                ll X = x + dx[i];
+                ll Y = y + dy[i];
                 if (valid(X, Y)) {
+                    vis[X][Y] = true;
                     bfs.emplace(X, Y);
                     dis[X][Y] = dis[x][y] + 1;
                 }
