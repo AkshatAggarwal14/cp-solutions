dec24d49b9d3ad81abf9a9d66c99a40d87549f11
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-12 17:19:00 +0530

    Add solution for problem `C - Dijkstra?` Link: https://codeforces.com/contest/20/problem/C Tags: graphs, shortest paths, *1900 Ref: https://codeforces.com/contest/20/submission/131637932

--- codeforces/20/C.cpp
+++ codeforces/20/C.cpp
@@ -1,6 +1,13 @@
+#ifndef ONLINE_JUDGE
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...) 42
+#endif
 using ll = int64_t;
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
 const ll inf = 1e18;
 
 void Solution() {
@@ -42,12 +49,16 @@ void Solution() {
     cout << '\n';
 }
 
+// clang-format off
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
     Solution();
+    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
