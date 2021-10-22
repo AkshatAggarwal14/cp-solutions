f69c985b785f27421c66bc2d7c8896db23129038
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-22 18:25:00 +0530

    Add solution for problem `D - Secret Passwords` Link: https://codeforces.com/contest/1263/problem/D Tags: dfs and similar, dsu, graphs, *1500 Ref: https://codeforces.com/contest/1263/submission/132696650

--- codeforces/1263/D.cpp
+++ codeforces/1263/D.cpp
@@ -1,4 +1,4 @@
-#ifdef LOCAL
+#ifndef ONLINE_JUDGE
 // https://github.com/AkshatAggarwal14/Competetive-Programming
 #include "Akshat.hpp"
 #else
@@ -6,69 +6,54 @@
 using namespace std;
 #define dbg(...)
 #endif
-using ll = long long;
-constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
-#define all(x) (x).begin(), (x).end()
-template <class T, class U = T>
-constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
-template <class T, class U = T>
-constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
-const ll MOD = 1e9 + 7;
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
 
-struct DSU {
-    int N;
-    vector<int> par, siz;
-    DSU(int n) : N(n), par(N), siz(N, 1) {
-        for (int i = 0; i < N; i++) par[i] = i;
-    }
-    int root(int X) {
-        if (par[X] != X) par[X] = root(par[X]);
-        return par[X];
-    }
-    bool same(int X, int Y) { return root(X) == root(Y); }
-    void join(int X, int Y) {
-        X = root(X), Y = root(Y);
-        if (X == Y) return;
-        if (siz[Y] < siz[X]) swap(X, Y);
-        par[X] = Y;
-        siz[Y] += siz[X];
-        siz[X] = 0;
-    }
-    int get_size(int X) { return siz[root(X)]; }
-    int components() {
-        int cnt = 0;
-        for (int i = 0; i < N; ++i) cnt += (root(i) == i);
-        return cnt;
-    }
-};
 void Solution() {
-    int n;
+    //! number of connected components from graph of index to characters
+    // 'a' --> n
+    // 'b' --> n + 1
+
+    ll n;
     cin >> n;
+    vector<vector<ll>> g(n + 26);  // 26 for the characters
+    vector<ll> vis(n + 26, 0);
+
     string s;
-    DSU dsu(n);
-    vector<int> occ(26, -1);
-    for (int i = 0; i < n; ++i) {
+    for (ll i = 0; i < n; ++i) {
         cin >> s;
         for (char &c : s) {
-            if (occ[c - 'a'] != -1)
-                dsu.join(occ[c - 'a'], i);
-            occ[c - 'a'] = i;
+            g[i].push_back(n + c - 'a');
+            g[n + c - 'a'].push_back(i);
         }
     }
-    cout << dsu.components() << '\n';
+    auto dfs = [&](const auto &self, ll parent) -> void {
+        vis[parent] = 1;
+        for (ll &child : g[parent]) {
+            if (vis[child]) continue;
+            self(self, child);
+        }
+    };
+    ll components = 0;
+    for (ll i = 0; i < n; ++i) {
+        if (vis[i]) continue;
+        dfs(dfs, i);
+        components++;
+    }
+
+    cout << components << '\n';
 }
 
+// clang-format off
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
-    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
     cout << fixed << setprecision(12);
-    int tc = 1;
-    // cin >> tc;
-    while (tc--) {
-        Solution();
-    }
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
+    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
