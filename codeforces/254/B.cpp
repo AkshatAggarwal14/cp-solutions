b3f1f8379db3ca2d1672bf3d659204f599c3aad5
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-23 09:56:00 +0530

    Add solution for problem `B - Jury Size` Link: https://codeforces.com/contest/254/problem/B Tags: brute force, implementation, *1500 Ref: https://codeforces.com/contest/254/submission/132744257

--- codeforces/254/B.cpp
+++ codeforces/254/B.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 using ll = int64_t;
 
-// vector<ll> y = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-// vector<ll> pref(13, 0);
-// partial_sum(y.begin(), y.end(), pref.begin() + 1);
-
 void Solution() {
-    vector<ll> pref = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+    vector<ll> y = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    vector<ll> pref(13, 0);
+    partial_sum(y.begin(), y.end(), pref.begin() + 1);
     vector<ll> vals(500, 0);
     auto getNum = [&](ll month, ll day) -> ll {
         return 100 + pref[month - 1] + day;
