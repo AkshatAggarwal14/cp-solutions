2261ef96a276396dc56d8585356e9f98d302edd6
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-23 09:48:00 +0530

    Add solution for problem `B - Jury Size` Link: https://codeforces.com/contest/254/problem/B Tags: brute force, implementation, *1500 Ref: https://codeforces.com/contest/254/submission/132743889

--- codeforces/254/B.cpp
+++ codeforces/254/B.cpp
@@ -1,17 +1,25 @@
+#ifndef ONLINE_JUDGE
 // https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#endif
 using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define ff first
+#define ss second
+
+array<ll, 12> y = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+array<ll, 13> pref;
+vector<ll> vals(500, 0);
+
+ll getNum(ll month, ll day) {
+    return 100 + pref[month - 1] + day;
+}
 
 void Solution() {
-    vector<ll> y = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    vector<ll> pref(13, 0);
-    partial_sum(y.begin(), y.end(), pref.begin() + 1);
-    vector<ll> vals(500, 0);
-    auto getNum = [&](ll month, ll day) -> ll {
-        return 100 + pref[month - 1] + day;
-    };
-
     ll n, m, d, p, t, xx;
     cin >> n;
     for (ll i = 0; i < n; ++i) {
@@ -19,6 +27,7 @@ void Solution() {
         xx = getNum(m, d) - 1;
         for (ll j = xx; j >= xx - t + 1; --j) vals[j] += p;
     }
+    dbg(vals);
     cout << *max_element(vals.begin(), vals.end()) << '\n';
 }
 
@@ -26,6 +35,7 @@ int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+    partial_sum(y.begin(), y.end(), pref.begin() + 1);
     Solution();
     return 0;
 }
