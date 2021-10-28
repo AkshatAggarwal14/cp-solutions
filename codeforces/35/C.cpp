c57f274cf18b73f9406fa23645fb2df68da81af5
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-28 23:46:00 +0530

    Add solution for problem `C - Fire Again` Link: https://codeforces.com/contest/35/problem/C Tags: brute force, dfs and similar, shortest paths, *1500 Ref: https://codeforces.com/contest/35/submission/133386620

--- codeforces/35/C.cpp
+++ codeforces/35/C.cpp
@@ -11,32 +11,31 @@ auto sz = [](const auto &container) -> ll { return container.size(); };
 
 void Solution() {
     ll n, m, k, u, v;
-    cin >> n >> m >> k;
-    auto get = [&](ll x, ll y) -> ll { return (x - 1) * m + (y - 1); };
-    auto getX = [&](ll d) -> ll { return (d / m + 1); };
-    auto getY = [&](ll d) -> ll { return (d % m + 1); };
+    auto get = [&](ll x, ll y) -> ll { return x * m + y; };
     queue<ll> bfs;
-    vector<ll> dist(n * m, -1);
+    cin >> n >> m >> k;
+    vector<vector<ll>> dist(n, vector<ll>(m, -1));
     for (ll i = 0; i < k; ++i) {
         cin >> u >> v;
-        bfs.push(get(u, v));  // pushing initial for multisource bfs
-        dist[get(u, v)] = 0;
+        bfs.push(get(u - 1, v - 1));  // pushing initial for multisource bfs
+        dist[u - 1][v - 1] = 0;
     }
     ll dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
     ll d = 0, mx = -1, my = -1;
     while (!bfs.empty()) {
-        ll x = getX(bfs.front()), y = getY(bfs.front());
+        ll x = bfs.front() / m, y = bfs.front() % m;
         bfs.pop();
         for (ll i = 0; i < 4; ++i) {
             ll nx = x + dx[i], ny = y + dy[i];
-            if (nx > n || ny > m || nx < 1 || ny < 1) continue;
-            if (dist[get(nx, ny)] != -1) continue;
+            if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
+            if (dist[nx][ny] != -1) continue;
             bfs.push(get(nx, ny));
-            dist[get(nx, ny)] = dist[get(x, y)] + 1;
+            dist[nx][ny] = dist[x][y] + 1;
         }
-        if (dist[get(x, y)] >= d) d = dist[get(x, y)], mx = x, my = y;
+        if (dist[x][y] >= d) d = dist[x][y], mx = x, my = y;
     }
-    cout << mx << ' ' << my << '\n';
+    dbg(dist);
+    cout << mx + 1 << ' ' << my + 1 << '\n';
 }
 
 // clang-format off
