b76ced83f7b43e2a55f246bf15ea00ecae27a04f
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-29 22:28:00 +0530

    Add solution for problem `B - Update Files` Link: https://codeforces.com/contest/1606/problem/B Tags: greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1606/submission/133524529

--- codeforces/1606/B.cpp
+++ codeforces/1606/B.cpp
@@ -1,33 +1,39 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
-
 using namespace std;
+#define dbg(...)
+#endif
 using ll = int64_t;
 auto sz = [](const auto &container) -> ll { return container.size(); };
 
-void Solution() {
-    ll n, k, p = 1;
-    cin >> n >> k;
-    n--;
-    ll ctr = 0;
-    for (ll i = 0; i < 62; ++i) {
-        if (p < k && p <= n) {
-            ctr++;
-            n -= p;
-        }
-        p *= 2;
-    }
-    cout << ctr + n / k + !!(n % k) << '\n';
+int64_t ceil_div(int64_t a, int64_t b) {
+    return a / b + ((a ^ b) > 0 && a % b != 0);
 }
 
+void Solution() {
+    ll n, k;
+    cin >> n >> k;
+    ll d = k;
+    ll cnt = 0;
+    while (d != 0) cnt++, d = d / 2;
+    ll po = static_cast<ll>(pow(2, cnt));
+    n -= po;
+    cout << cnt + ceil_div(n, k) << '\n';
+}
+
+// clang-format off
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 #ifdef LOCAL
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    ll tc;
-    cin >> tc;
-    while (tc--)
-        Solution();
+    cout << fixed << setprecision(12);
+    ll tc; cin >> tc; while (tc--)
+    Solution();
+    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
