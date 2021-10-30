f5e0460826a54212f4f19a02951b9876d6789121
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-10-30 18:38:00 +0530

    Add solution for problem `D - Play Train` Link: https://atcoder.jp/contests/abc225/tasks/abc225_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc225/submissions/26922262

--- atcoder/abc225/D.cpp
+++ atcoder/abc225/D.cpp
@@ -1,29 +1,46 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
-auto sz = [](const auto &container) -> int { return int(container.size()); };
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
 
 void Solution() {
-    int n, q, type, x, y;
+    ll n, q, type, x, y;
     cin >> n >> q;
-    vector<int> parent(n + 1, -1), child(n + 1, -1), temp;
-    for (int i = 1; i <= n; ++i) parent[i] = i;
+    vector<ll> parent(n), child(n);
+    for (ll i = 0; i < n; ++i) parent[i] = i, child[i] = -1;
     while (q--) {
         cin >> type;
+        dbg(parent, child);
         if (type == 3) {
             cin >> x;
-            temp.clear();
-            while (x != parent[x]) x = parent[x];  // go to the first car
-            while (child[x] != -1) temp.push_back(x), x = child[x];
-            temp.push_back(x);
+            x--;
+            ll start = x;
+            vector<ll> temp;
+            while (start != parent[start]) start = parent[start];
+            while (child[start] != -1) {
+                temp.push_back(start + 1);
+                start = child[start];
+            }
+            temp.push_back(start + 1);
             cout << sz(temp) << ' ';
-            for (int &ele : temp) cout << ele << ' ';
+            for (ll &ele : temp) cout << ele << ' ';
             cout << '\n';
         } else {
             cin >> x >> y;
-            if (type == 1)
-                parent[y] = x, child[x] = y;
-            else
-                parent[y] = y, child[x] = -1;
+            x--, y--;
+            if (type == 1) {
+                parent[y] = x;
+                child[x] = y;
+            } else {
+                parent[y] = y;
+                child[x] = -1;
+            }
         }
     }
 }
@@ -35,6 +52,9 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
     Solution();
+    cerr << "Time:" << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
