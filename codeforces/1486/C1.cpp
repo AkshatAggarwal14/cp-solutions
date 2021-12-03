614eaa9b39daccd308ebed7d7652812785c77670
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-04 01:49:00 +0530

    Add solution for problem `C1 - Guessing the Greatest (easy version)` Link: https://codeforces.com/contest/1486/problem/C1 Tags: binary search, interactive, *1600 Ref: https://codeforces.com/contest/1486/submission/137923956

--- codeforces/1486/C1.cpp
+++ codeforces/1486/C1.cpp
@@ -33,10 +33,10 @@ auto find_last_true(auto l, auto r, const auto &p) {
 
 ll query(ll l, ll r) {
     if (l >= r) return -1;
-    cout << "? " << l << ' ' << r << endl;
+    cout << "? " << l + 1 << ' ' << r + 1 << endl;
     ll res;
     cin >> res;
-    return res;
+    return res - 1;
 }
 
 // if position is same, then ans in this segment, else next segment.
@@ -44,14 +44,14 @@ ll query(ll l, ll r) {
 void Solution() {
     ll n;
     cin >> n;
-    ll smax = query(1, n), ans = -1;
-    if (smax == 1 || query(1, smax) != smax) {
+    ll smax = query(0, n - 1), ans = -1;
+    if (smax == 0 || query(0, smax) != smax) {
         // if 0 or q[0, smax] is smaller than smax
         // it is the first index after smax then returns same res for query
-        ans = find_first_false(smax, n, [&](ll m) { return (query(smax, m) != smax); });
+        ans = find_first_false(smax, n - 2, [&](ll m) { return (query(smax, m) != smax); }) + 1;
     } else {
         // it is the largest index less than smax, where res is same as query
-        ans = find_last_true(1LL, smax, [&](ll m) { return (query(m, smax) == smax); });
+        ans = find_last_true(1LL, smax, [&](ll m) { return (query(m, smax) == smax); }) + 1;
     }
     cout << "! " << ans << endl;
 }
