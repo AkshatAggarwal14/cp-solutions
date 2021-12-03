3cec71a8a151ff38d5f314bc5f8917f5764929ca
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-03 13:54:00 +0530

    Add solution for problem `C - Restore Graph` Link: https://codeforces.com/contest/404/problem/C Tags: dfs and similar, graphs, sortings, *1800 Ref: https://codeforces.com/contest/404/submission/137863343

--- codeforces/404/C.cpp
+++ codeforces/404/C.cpp
@@ -17,24 +17,22 @@ bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 void Solution() {
     ll n, k, p = 1;
     cin >> n >> k;
-    vector<bool> mark(n, false);
     vector<ll> d(n), cnt(n, 0), deg(n, 0);
     for (ll &x : d) cin >> x, ++cnt[x];
     ll mx = *ranges::max_element(d);
+    // 1, k, k^2, ... <- max nodes with d[0], d[1] ...
     for (ll i = 0; i < n; ++i) {
         if (!cnt[i] || p >= (ll)1e5) break;
         if (cnt[i] > p) return void(cout << "-1\n");
-        mark[i] = true;
         p *= k;
     }
+    // distances 0 1 2 3.. all consecutive should exist
     vector<ll> check(d);
     sort(all(check));
     check.resize(unique(all(check)) - check.begin());
-    for (ll i = 1; i < sz(check); ++i) {
-        if (check[i] != check[i - 1] + 1) {
-            return void(cout << "-1\n");
-        }
-    }
+    for (ll i = 1; i < sz(check); ++i)
+        if (check[i] != check[i - 1] + 1) return void(cout << "-1\n");
+    // joining all children to their parents equally.. %sz(parent)..
     vector<vector<ll>> avail(mx + 1);
     vector<pair<ll, ll>> edges;
     for (ll i = 0; i < n; ++i) avail[d[i]].push_back(i);
@@ -42,15 +40,16 @@ void Solution() {
         ll parent = i - 1, child = i;
         if (avail[parent].empty()) break;
         for (ll j = 0; j < sz(avail[child]); ++j) {
-            edges.push_back({avail[parent][j % sz(avail[parent])], avail[child][j]});
+            ll u = avail[parent][j % sz(avail[parent])];
+            ll v = avail[child][j];
+            ++deg[u], ++deg[v];
+            edges.push_back({u, v});
         }
     }
-    if (edges.empty()) return void(cout << "-1\n");
-    for (auto &[x, y] : edges) ++deg[x], ++deg[y];
-    if (*ranges::max_element(deg) > k) return void(cout << "-1\n");
+    // rechecking at the end
+    if (*ranges::max_element(deg) > k || edges.empty()) return void(cout << "-1\n");
     cout << sz(edges) << '\n';
     for (auto &[x, y] : edges) cout << x + 1 << ' ' << y + 1 << '\n';
-    dbg(edges);
 }
 
 // clang-format off
