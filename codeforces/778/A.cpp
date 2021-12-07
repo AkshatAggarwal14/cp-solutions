2800e38d7e0f2f3ee937d22c9305593ecbeb66d7
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-07 20:54:00 +0530

    Add solution for problem `A - String Game` Link: https://codeforces.com/contest/778/problem/A Tags: binary search, greedy, strings, *1700 Ref: https://codeforces.com/contest/778/submission/138360435

--- codeforces/778/A.cpp
+++ codeforces/778/A.cpp
@@ -38,8 +38,12 @@ void Solution() {
     vector<ll> idx(n);
     for (ll &i : idx) cin >> i;
     cout << find_last_true(1LL, n, [&](ll M) {  // bin search
-        string s(t);
-        for (ll i = 0; i < M; ++i) s[idx[i] - 1] = '.';  // marked blocked
+        string s = "";
+        vector<bool> mark(sz(t), true);
+        for (ll i = 0; i < M; ++i) mark[idx[i] - 1] = false;
+        for (ll i = 0; i < n; ++i)
+            if (mark[i]) s += t[i];  // build string after M moves.
+
         // check subsequence, as if subsequence then he can continue himself.
         ll pos = 0;
         for (ll i = 0; i < sz(s); ++i)
