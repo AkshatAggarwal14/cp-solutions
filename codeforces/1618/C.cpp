a3807f755eac9baa6ccf8b29e6a75d028a4ded72
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-14 23:03:00 +0530

    Add solution for problem `C - Paint the Array` Link: https://codeforces.com/contest/1618/problem/C Tags: math, *1100 Ref: https://codeforces.com/contest/1618/submission/139316669

--- codeforces/1618/C.cpp
+++ codeforces/1618/C.cpp
@@ -18,20 +18,33 @@ void Solution() {
     ll n;
     cin >> n;
     vector<ll> a(n);
-    for (ll &x : a) cin >> x;
+    for (auto &x : a) cin >> x;
     ll g1 = a[0];
     for (ll i = 0; i < n; i += 2) g1 = gcd(g1, a[i]);
+    bool flag = true;
     for (ll i = 1; i < n; i += 2) {
         if (a[i] % g1 == 0) {
-            ll g2 = a[1];
-            for (ll j = 1; j < n; j += 2) g2 = gcd(g2, a[j]);
-            for (ll j = 0; j < n; j += 2) {
-                if (a[j] % g2 == 0) return void(cout << "0\n");
-            }
-            return void(cout << g2 << '\n');
+            flag = false;
+            break;
         }
     }
-    cout << g1 << '\n';
+    if (flag) {
+        cout << g1 << '\n';
+    } else {
+        ll g2 = a[1];
+        for (ll i = 1; i < n; i += 2) g2 = gcd(g2, a[i]);
+        flag = true;
+        for (ll i = 0; i < n; i += 2) {
+            if (a[i] % g2 == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cout << g2 << '\n';
+        } else
+            cout << "0\n";
+    }
 }
 
 // clang-format off
