255943ebb14762514c57a6dcc3aa7e577596537d
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-15 16:27:00 +0530

    Add solution for problem `D - MEX maximizing` Link: https://codeforces.com/contest/1294/problem/D Tags: data structures, greedy, implementation, math, *1600 Ref: https://codeforces.com/contest/1294/submission/139381600

--- codeforces/1294/D.cpp
+++ codeforces/1294/D.cpp
@@ -1,24 +1,47 @@
+#ifdef LOCAL
 // https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
-using ll = int;
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-    ll q, x, num, V;
+void Solution() {
+    ll q, x;
     cin >> q >> x;
     vector<set<ll>> MEX(x);
     set<ll> vals;
     for (ll i = 0; i < q; ++i) MEX[i % x].insert(i), vals.insert(i);
     vals.insert(q);
     while (q--) {
+        ll num;
         cin >> num;
         if (!MEX[num % x].empty()) {
-            V = *MEX[num % x].begin();
+            ll V = *MEX[num % x].begin();
             vals.erase(V);
             MEX[num % x].erase(V);
         }
         cout << *vals.begin() << '\n';
     }
+}
+
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
     return 0;
 }
