29855ff4b5ee98a4d9894efe76a75809eb69196e
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-15 18:18:00 +0530

    Add solution for problem `J - Jeopardy of Dropped Balls` Link: https://codeforces.com/contest/1575/problem/J Tags: binary search, brute force, dsu, implementation, *1500 Ref: https://codeforces.com/contest/1575/submission/139391986

--- codeforces/1575/J.cpp
+++ codeforces/1575/J.cpp
@@ -10,24 +10,16 @@ void Solution() {
     for (auto &x : grid)
         for (auto &y : x) cin >> y;
     ll dx[] = {0, 1, 0}, dy[] = {1, 0, -1};
-    // function<void(ll, ll)> dfs = [&](ll x, ll y) {
-    //     if (x == n) return void(cout << y + 1 << ' ');
-    //     ll t = grid[x][y] - '0';
-    //     grid[x][y] = '2';
-    //     dfs(x + dx[t - 1], y + dy[t - 1]);
-    // };
+    function<void(ll, ll)> dfs = [&](ll x, ll y) {
+        if (x == n) return void(cout << y + 1 << ' ');
+        ll t = grid[x][y] - '0';
+        grid[x][y] = '2';
+        dfs(x + dx[t - 1], y + dy[t - 1]);
+    };
     for (ll i = 0; i < k; ++i) {
-        ll y, x = 0;
-        cin >> y;
-        --y;
-        while (x < n) {
-            ll t = grid[x][y] - '0';
-            grid[x][y] = '2';
-            x += dx[t - 1];
-            y += dy[t - 1];
-        }
-        cout << y + 1 << ' ';
-        // dfs(0, y - 1);
+        ll col;
+        cin >> col;
+        dfs(0, col - 1);
     }
 }
 
