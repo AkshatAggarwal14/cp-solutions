2483567a8b82114e0c216d33fbc3aaf495d64ac6
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-16 21:51:00 +0530

    Add solution for problem `D1 - Too Many Impostors (easy version)` Link: https://codeforces.com/contest/1617/problem/D1 Tags: constructive algorithms, implementation, interactive, *1800 Ref: https://codeforces.com/contest/1617/submission/139553127

--- codeforces/1617/D1.cpp
+++ codeforces/1617/D1.cpp
@@ -14,7 +14,7 @@ bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
 template <class T, class U = T>
 bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 
-ll query(ll a, ll b, ll c) {
+int query(int a, int b, int c) {
     cout << "? " << a + 1 << ' ' << b + 1 << ' ' << c + 1 << endl;
     ll res;
     cin >> res;
@@ -23,35 +23,43 @@ ll query(ll a, ll b, ll c) {
 }
 
 void Solution() {
-    ll n;
+    int n;
     cin >> n;
-    vector<ll> queries(n);
-    for (ll i = 0; i < n; ++i) queries[i] = query(i, (i + 1) % n, (i + 2) % n);
-    ll a = 0, b = 0, c = 0, d = 0;
-    for (ll i = 0; i < n; ++i) {
-        ll nxt = (i + 1) % n;
-        if (queries[i] ^ queries[nxt]) {  // 1 of a or b is impostor
+    vector<int> res(n);
+    for (int i = 0; i < n; ++i) {
+        res[i] = query(i, (i + 1) % n, (i + 2) % n);
+    }
+    int a, b;
+    for (int i = 0; i < n; ++i) {
+        int nxt = (i + 1) % n;
+        if (res[i] ^ res[nxt]) {
             a = (i + 1) % n;
             b = (i + 2) % n;
         }
     }
-    vector<ll> mark(n);  // 1 -> impostor
-    for (ll i = 0; i < n; ++i) {
+    vector<int> ans(n);
+    for (int i = 0; i < n; ++i) {
         if (i == a or i == b) continue;
-        mark[i] = query(a, b, i);  // if i is impostor, ans[i] = 1, else 0
+        ans[i] = query(a, b, i);
     }
-    for (ll i = 0; i < n; ++i) {
+    int c, d;
+    for (int i = 0; i < n; ++i) {
         if (i == a or i == b) continue;
-        if (mark[i] == 1) c = i;  // impostor
-        if (mark[i] == 0) d = i;  // crewmate
+        if (ans[i] == 1) {
+            c = i;
+        }
+        if (ans[i] == 0) {
+            d = i;
+        }
     }
-    mark[a] = query(c, d, a);  // determine a,b using c,d
-    mark[b] = query(c, d, b);
-    vector<ll> res;
-    for (ll i = 0; i < n; ++i)
-        if (mark[i]) res.push_back(i + 1);  // push impostors in vector
-    cout << "! " << sz(res) << ' ';
-    for (ll &i : res) cout << i << ' ';
+    ans[a] = query(c, d, a);
+    ans[b] = query(c, d, b);
+    vector<int> x;
+    for (int i = 0; i < n; ++i) {
+        if (ans[i]) x.push_back(i + 1);
+    }
+    cout << "! " << x.size() << ' ';
+    for (int i : x) cout << i << ' ';
     cout << endl;
 }
 
