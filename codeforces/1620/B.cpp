4ebe2783d27e90cf41f0483df87a96969574e45b
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-18 21:41:00 +0530

    Add solution for problem `B - Triangles on a Rectangle` Link: https://codeforces.com/contest/1620/problem/B Tags: geometry, greedy, math, *1000 Ref: https://codeforces.com/contest/1620/submission/139782258

--- codeforces/1620/B.cpp
+++ codeforces/1620/B.cpp
@@ -1,50 +1,33 @@
-#ifdef LOCAL
-// https://github.com/AkshatAggarwal14/Competetive-Programming
-#include "Akshat.hpp"
-#else
 #include "bits/stdc++.h"
 using namespace std;
-#define dbg(...)
-#endif
 using ll = int64_t;
-auto sz = [](const auto &container) -> ll { return container.size(); };
-#define all(x) (x).begin(), (x).end()
-template <class T, class U = T>
-bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
-template <class T, class U = T>
-bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 
-void Solution() {
-    ll w, h;
-    cin >> w >> h;
-    ll ans = 0;
-    for (ll i = 0; i < 2; ++i) {
-        ll k;
-        cin >> k;
-        vector<ll> x(k);
-        for (ll j = 0; j < k; ++j) cin >> x[j];
-        ans = max(ans, (x.back() - x.front()) * h);  // area = base * height
-    }
-    swap(w, h);
-    for (ll i = 0; i < 2; ++i) {
-        ll k;
-        cin >> k;
-        vector<ll> x(k);
-        for (ll j = 0; j < k; ++j) cin >> x[j];
-        ans = max(ans, (x.back() - x.front()) * h);
-    }
-    cout << ans << '\n';
+ll area(ll x1, ll x2, ll x3, ll y1, ll y2, ll y3) {
+    return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
 }
 
-// clang-format off
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    cout << fixed << setprecision(12);
-    ll tc; cin >> tc; while (tc--)
-    Solution();
+    ll tc;
+    cin >> tc;
+    ll w, h, x, y, k1, k2, k3, k4;
+    while (tc--) {
+        vector<array<ll, 2>> v1, v2, v3, v4;
+        ll res = 0;
+        cin >> w >> h;
+        cin >> k1;
+        for (ll i = 0; i < k1; ++i) cin >> x, v1.push_back({x, 0});
+        cin >> k2;
+        for (ll i = 0; i < k2; ++i) cin >> x, v2.push_back({x, h});
+        cin >> k3;
+        for (ll i = 0; i < k3; ++i) cin >> y, v3.push_back({0, y});
+        cin >> k4;
+        for (ll i = 0; i < k4; ++i) cin >> y, v4.push_back({w, y});
+        for (ll i = 0; i < k1; ++i) res = max(res, area(v2[0][0], v2[k2 - 1][0], v1[i][0], v2[0][1], v2[k2 - 1][1], v1[i][1]));
+        for (ll i = 0; i < k2; ++i) res = max(res, area(v1[0][0], v1[k1 - 1][0], v2[i][0], v1[0][1], v1[k1 - 1][1], v2[i][1]));
+        for (ll i = 0; i < k3; ++i) res = max(res, area(v4[0][0], v4[k4 - 1][0], v3[i][0], v4[0][1], v4[k4 - 1][1], v3[i][1]));
+        for (ll i = 0; i < k4; ++i) res = max(res, area(v3[0][0], v3[k3 - 1][0], v4[i][0], v3[0][1], v3[k3 - 1][1], v4[i][1]));
+        cout << res << '\n';
+    }
     return 0;
 }
