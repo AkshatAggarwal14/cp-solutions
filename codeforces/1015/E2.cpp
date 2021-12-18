f26e4ffb8642a264ba002d81b4a2390cad31399a
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-18 13:24:00 +0530

    Add solution for problem `E2 - Stars Drawing (Hard Edition)` Link: https://codeforces.com/contest/1015/problem/E2 Tags: binary search, dp, greedy, *1900 Ref: https://codeforces.com/contest/1015/submission/139716346

--- codeforces/1015/E2.cpp
+++ codeforces/1015/E2.cpp
@@ -1,12 +1,20 @@
+#ifdef LOCAL
 // https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#endif
 using ll = int64_t;
 auto sz = [](const auto &container) -> ll { return container.size(); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a ? a = std::forward<U>(b), true : false; }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b ? a = std::forward<U>(b), true : false; }
 
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-
+void Solution() {
     ll n, m;
     cin >> n >> m;
     vector<vector<char>> grid(n, vector<char>(m));
@@ -47,9 +55,20 @@ int main() {
     }
     for (ll i = 0; i < n; ++i)
         for (ll j = 0; j < m; ++j)
-            if (grid[i][j] == '*' && !mark[i][j]) return cout << "-1\n", 0;
+            if (grid[i][j] == '*' && !mark[i][j]) return void(cout << "-1\n");
     cout << sz(res) << '\n';
     for (auto &[a, b, c] : res) cout << a + 1 << ' ' << b + 1 << ' ' << c << '\n';
+}
 
+// clang-format off
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    // ll tc; cin >> tc; while (tc--)
+    Solution();
     return 0;
 }
