46bd8644445d7d5a4421634f7dd18b50bb87b13f
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-24 00:11:00 +0530

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/140381890

--- codeforces/1450/A.cpp
+++ codeforces/1450/A.cpp
@@ -1,33 +1,39 @@
-//*use cout<<"\n" instead of endl as endl is slower
-#include <bits/stdc++.h>
-
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
 using namespace std;
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define repeq(i, a, b) for (int i = a; i <= b; i++)
-#define repn(i, n) for (int i = 0; i < n; i++)
+#define dbg(...)
+#endif
+using ll = int64_t;
+auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const int MOD = 1e9 + 7;
 
-typedef long long ll;
-//SORT STRING TO SOLVE
-void solve()
-{
+void Solution() {
     ll n;
-    cin >> n;
     string s;
-    cin >> s;
-    sort(s.begin(), s.end());
-    cout << s << "\n";
-    return;
+    cin >> n >> s;
+    sort(all(s));
+    cout << s << '\n';
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        solve();
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        Solution();
     }
     return 0;
 }
