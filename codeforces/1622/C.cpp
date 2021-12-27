d8ec756787045c6c5d18fb38245f31503b382ee9
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-27 23:25:00 +0530

    Add solution for problem `C - Set or Decrease` Link: https://codeforces.com/contest/1622/problem/C Tags: binary search, brute force, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1622/submission/140830872

--- codeforces/1622/C.cpp
+++ codeforces/1622/C.cpp
@@ -15,6 +15,23 @@ template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
 
+template <bool b>
+auto binsearch(auto l, auto r, const auto &pred) {
+    --l, ++r;
+    for (decltype(l) m; m = midpoint(l, r), r > l + 1;) (pred(m) ? l : r) = m;
+    return (b ? l : r);
+}
+
+// returns first i in [l, r], p(i) false, and if none found, returns r + 1
+auto find_first_false(auto l, auto r, const auto &p) {
+    return binsearch<false>(l, r, p);
+}
+
+// returns last i in [l, r], p(i) true, and if none found, returns l - 1
+auto find_last_true(auto l, auto r, const auto &p) {
+    return binsearch<true>(l, r, p);
+}
+
 void Solution() {
     ll n, k;
     cin >> n >> k;
