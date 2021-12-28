3107e7a6c41d47c4bde782abad8a6f253caedcdc
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2021-12-28 19:41:00 +0530

    Add solution for problem `B - Game on Ranges` Link: https://codeforces.com/contest/1623/problem/B Tags: brute force, dfs and similar, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1623/submission/140917008

--- codeforces/1623/B.cpp
+++ codeforces/1623/B.cpp
@@ -20,8 +20,10 @@ void Solution() {
     cin >> n;
     vector<array<ll, 2>> v(n);
     for (auto &[x, y] : v) cin >> x >> y;
-    // sort by length of intervals
-    sort(all(v), [&](array<ll, 2> &A, array<ll, 2> &B) { return A[1] - A[0] > B[1] - B[0]; });
+    sort(all(v), [&](array<ll, 2> &A, array<ll, 2> &B) {
+        return A[1] - A[0] > B[1] - B[0];
+    });
+    dbg(v);
     vector<array<ll, 3>> ans;
     for (ll i = 0; i < n; ++i) {
         if (v[i][0] == v[i][1]) {
@@ -29,13 +31,25 @@ void Solution() {
             continue;
         }
         ll j1 = i + 1, j2 = i + 1;
-        for (; j1 < n; ++j1)
-            if (v[j1][0] == v[i][0]) break;
-        for (; j2 < n; ++j2)
-            if (v[j2][1] == v[i][1]) break;
-        assert(!(j1 == n && j2 == n));  // definitely someone exists
-        // found same y
-        ans.push_back({v[i][0], v[i][1], ((j1 == n) ? (v[j2][0] - 1) : (v[j1][1] + 1))});
+        for (; j1 < n; ++j1) {
+            if (v[j1][0] == v[i][0]) {
+                break;
+            }
+        }
+        for (; j2 < n; ++j2) {
+            if (v[j2][1] == v[i][1]) {
+                break;
+            }
+        }
+        assert(!(j1 == n && j2 == n));
+        if (j1 == n) {
+            // found same y
+            ans.push_back({v[i][0], v[i][1], v[j2][0] - 1});
+            continue;
+        } else {
+            ans.push_back({v[i][0], v[i][1], v[j1][1] + 1});
+            continue;
+        }
     }
     for (auto &[a, b, c] : ans) cout << a << ' ' << b << ' ' << c << '\n';
     cout << '\n';
