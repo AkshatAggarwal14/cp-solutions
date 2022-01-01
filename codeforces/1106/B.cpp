11ce2e58d0664b204ff168ac531faee7deeabeeb
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-01 17:33:00 +0530

    Add solution for problem `B - Lunar New Year and Food Ordering` Link: https://codeforces.com/contest/1106/problem/B Tags: data structures, implementation, *1500 Ref: https://codeforces.com/contest/1106/submission/141311262

--- codeforces/1106/B.cpp
+++ codeforces/1106/B.cpp
@@ -15,31 +15,40 @@ template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
 
+vector<ll> quantity, cost;
+struct cmp {
+    bool operator()(const ll &A, const ll &B) const {
+        if (cost[A] == cost[B]) return A < B;
+        return cost[A] < cost[B];
+    }
+};
+
 void Solution() {
     ll n, m, dish_number, amount, temp;
     cin >> n >> m;
-    vector<ll> quantity(n), cost(n);
-    set<pair<ll, ll>> dishes;  // {cost, dish_type}
-    for (ll i = 0; i < n; ++i) cin >> quantity[i];
-    for (ll i = 0; i < n; ++i) cin >> cost[i], dishes.insert({cost[i], i});
+    quantity.assign(n + 1, -1), cost.assign(n + 1, -1);
+    for (ll i = 1; i <= n; ++i) cin >> quantity[i];
+    for (ll i = 1; i <= n; ++i) cin >> cost[i];
+    map<ll, ll, cmp> dishes;  // map sorted by cost and then index
+    for (ll i = 1; i <= n; ++i) dishes[i] = quantity[i];
 
     // returns how much removed
     auto remove = [&](const ll &what, const ll &how_much) -> ll {
-        if (!dishes.contains({cost[what], what})) return 0;
-        ll t = min(how_much, quantity[what]);
-        quantity[what] -= t;
-        if (quantity[what] == 0) dishes.erase({cost[what], what});
+        if (!dishes.contains(what)) return 0;
+        ll t = min(how_much, dishes[what]);
+        dishes[what] -= t;
+        if (dishes[what] == 0) dishes.erase(what);
         return t;
     };
 
     while (m--) {
         cin >> dish_number >> amount;
         ll res = 0;
-        temp = remove(dish_number - 1, amount);
-        res += temp * cost[dish_number - 1];
+        temp = remove(dish_number, amount);
+        res += temp * cost[dish_number];
         amount -= temp;  // remove present
         while (amount > 0 && !dishes.empty()) {
-            ll dish_id = dishes.begin()->second;
+            ll dish_id = dishes.begin()->first;
             temp = remove(dish_id, amount);
             res += temp * cost[dish_id];
             amount -= temp;
