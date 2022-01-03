219fc0e9398b6d831c3d2a783ee68c5877c8cba0
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-04 00:06:00 +0530

    Add solution for problem `C - Hidden Permutations` Link: https://codeforces.com/contest/1621/problem/C Tags: dfs and similar, interactive, math, *1700 Ref: https://codeforces.com/contest/1621/submission/141579008

--- codeforces/1621/C.cpp
+++ codeforces/1621/C.cpp
@@ -15,30 +15,30 @@ template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
 
+ll query(const ll &index) {
+    cout << "? " << index + 1 << endl;
+    ll res;
+    cin >> res;
+    if (res == 0) exit(0);
+    return res - 1;
+}
+
 // a permutation is a group of cycles
 void Solution() {
-    int n;
+    ll n;
     cin >> n;
-    vector<int> ans(n, -1);
-    int prev = -1, nxt = -1;
-    auto ask = [&](const int &idx) -> int {
-        cout << "? " << idx + 1 << endl;
-        int res;
-        cin >> res;
-        if (res == 0) exit(0);
-        return res - 1;
-    };
-
-    for (int i = 0; i < n; ++i) {
-        if (ans[i] != -1) continue;  // already visited
-        prev = ask(i);
-        while (ans[prev] == -1) {  // until ans[prev] is not set
-            nxt = ask(i);          // ask next
-            ans[prev] = nxt;       // set nxt
+    vector<ll> ans(n, -1);
+    ll prev = -1, nxt = -1;
+    for (ll i = 0; i < n; ++i) {
+        if (ans[i] != -1) continue;
+        prev = query(i);
+        while (ans[prev] == -1) {
+            nxt = query(i);
+            ans[prev] = nxt;
+            if (prev == nxt) break;
             prev = nxt;
         }
     }
-
     cout << "! ";
     for (auto &a : ans) cout << a + 1 << ' ';
     cout << endl;
