385666a64dec857a1ba77092d3f768738b66c2e8
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-11 14:57:00 +0530

    Add solution for problem `G - MinOr Tree` Link: https://codeforces.com/contest/1624/problem/G Tags: bitmasks, dfs and similar, dsu, graphs, greedy, *1900 Ref: https://codeforces.com/contest/1624/submission/142350571

--- codeforces/1624/G.cpp
+++ codeforces/1624/G.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 #define dbg(...)
 #endif
-using ll = int64_t;
+using ll = int;
 constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
 #define all(x) (x).begin(), (x).end()
 template <class T, class U = T>
@@ -14,19 +14,6 @@ constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true
 template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
-
-template <class T>
-struct simple_queue {
-    std::vector<T> payload;
-    int pos = 0;
-    void reserve(int n) { payload.reserve(n); }
-    int size() const { return int(payload.size()) - pos; }
-    bool empty() const { return pos == int(payload.size()); }
-    void push(const T &t) { payload.push_back(t); }
-    T &front() { return payload[pos]; }
-    void clear() { payload.clear(), pos = 0; }
-    void pop() { pos++; }
-};
 
 void Solution() {
     ll n, m;
@@ -37,19 +24,21 @@ void Solution() {
     vector<basic_string<ll>> g(n);
     // to check connected
     auto connected = [&](const vector<array<ll, 3>> &Edges) -> bool {
-        for (auto &x : g) x.clear();  // faster
-        for (const auto &[u, v, w] : Edges) g[u].push_back(v), g[v].push_back(u);
+        for (auto &x : g) x.clear();
+        for (auto &[u, v, w] : Edges) g[u].push_back(v), g[v].push_back(u);
         fill(all(vis), false);
-        simple_queue<ll> bfs;
-        bfs.push(0);
+        // queue<ll> bfs;
+        vector<ll> bfs(n, -1);
+        ll curr = 0, end = 0;
+        bfs[end++] = 0;
         vis[0] = true;
-        while (!bfs.empty()) {
-            ll node = bfs.front();
-            bfs.pop();
+        while (curr < end) {
+            ll node = bfs[curr++];
             for (ll &child : g[node]) {
                 if (vis[child]) continue;
                 vis[child] = true;
-                bfs.push(child);
+                bfs[end++] = child;
+                // bfs.push_back(child);
             }
         }
         return (count(all(vis), true) == n);
