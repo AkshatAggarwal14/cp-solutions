a821a52539c8de09fb688f9ceb0e0d49f6e9ac46
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-15 23:48:00 +0530

    Add solution for problem `C - The Kth Time Query` Link: https://atcoder.jp/contests/abc235/tasks/abc235_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc235/submissions/28572583

---
+++ atcoder/abc235/C.cpp
@@ -0,0 +1,47 @@
+#ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
+using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+
+void Solution() {
+    ll n, q, x, k;
+    cin >> n >> q;
+    vector<ll> a(n);
+    map<ll, vector<ll>> mp;
+    for (ll i = 0; i < n; ++i) cin >> a[i], mp[a[i]].push_back(i);
+    while (q--) {
+        cin >> x >> k, --k;
+        // kth occurence of x
+        if (!mp.count(x) || sz(mp[x]) <= k)
+            cout << "-1\n";
+        else
+            cout << mp[x][k] + 1 << '\n';
+    }
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        Solution();
+    }
+    return 0;
+}
