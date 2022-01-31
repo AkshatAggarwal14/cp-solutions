49725909edb45813c9ac76234afd2f242188a0c8
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-31 10:35:00 +0530

    Add solution for problem `F - Array Partition` Link: https://codeforces.com/contest/1454/problem/F Tags: binary search, data structures, greedy, two pointers, *2100 Ref: https://codeforces.com/contest/1454/submission/144613334

--- codeforces/1454/F.cpp
+++ codeforces/1454/F.cpp
@@ -51,18 +51,13 @@ void Solution() {
     cin >> n;
     vector<ll> a(n + 1);
     for (int i = 1; i <= n; ++i) cin >> a[i];
-    Sparse_Table<ll> MIN(a, [](ll A, ll B) { return min(A, B); });
-    // dont need sparse table for MAX, only need pref and suff
-    vector<ll> pref_MAX(n + 1), suff_MAX(n + 1);
-    pref_MAX[1] = a[1], suff_MAX[n] = a[n];
-    for (ll i = 2; i <= n; ++i) pref_MAX[i] = max(pref_MAX[i - 1], a[i]);
-    for (ll i = n - 1; i >= 1; --i) suff_MAX[i] = max(suff_MAX[i + 1], a[i]);
+    Sparse_Table<ll> MAX(a, [](ll A, ll B) { return max(A, B); }),
+        MIN(a, [](ll A, ll B) { return min(A, B); });
 
     // fix x, then BS for y
     for (int x = 1; x <= n; ++x) {
-        int l = x + 1, r = n - 1;
-        --l, ++r;
-        ll max_x = pref_MAX[x];  //! [1, x]
+        int l = x + 1, r = n + 1;  // x + 1 to n
+        ll max_x = MAX.query(1, x);
         //   max  |       min      |    max
         // 1 .. x | x+1 x+2 .. m-1 | m m+1 .. n
 
@@ -70,11 +65,11 @@ void Solution() {
         // from [x+1, m] U [m+1, n] to [x+1, m+1] U [m+2, n], min_y can decrease and max_z can decrease
         while (r > l + 1) {
             int m = (l + r) / 2;
-            ll min_y = MIN.query(x + 1, m);  //! [x+1, m]
-            ll max_z = suff_MAX[m + 1];      //! [m+1, n]
+            ll min_y = MIN.query(x + 1, m - 1);
+            ll max_z = MAX.query(m, n);
             if (max_x == min_y && max_x == max_z) {
                 cout << "YES\n";
-                cout << x << ' ' << (m - x) << ' ' << (n - m) << '\n';
+                cout << x << ' ' << (m - x - 1) << ' ' << (n - (m - 1)) << '\n';
                 return;
             }
 
