d0b7f96d464e2693e00ceb36f797be7e6453fb03
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-01-31 23:10:00 +0530

    Add solution for problem `D - Make Them Equal` Link: https://codeforces.com/contest/1633/problem/D Tags: dp, greedy, *1600 Ref: https://codeforces.com/contest/1633/submission/144766513

--- codeforces/1633/D.cpp
+++ codeforces/1633/D.cpp
@@ -1,4 +1,5 @@
 #ifdef LOCAL
+// https://github.com/AkshatAggarwal14/Competetive-Programming
 #include "Akshat.hpp"
 #else
 #include "bits/stdc++.h"
@@ -6,13 +7,23 @@ using namespace std;
 #define dbg(...)
 #endif
 using ll = long long;
-const ll N = 1005;
-const ll INF = 1e18;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
+#define all(x) (x).begin(), (x).end()
+template <class T, class U = T>
+constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
+template <class T, class U = T>
+constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
+const ll MOD = 1e9 + 7;
+const ll N = 1000;
+vector<ll> operations(N + 1, LLONG_MAX);
+
+mt19937 rng(uint32_t(chrono::steady_clock::now().time_since_epoch().count()));
+#define uid(a, b) uniform_int_distribution<ll>(a, b)(rng)
 
 ll knapsack(ll W, vector<ll> weight, vector<ll> value) {
     // making and initializing dp array
-    ll n = ll(weight.size());
-    assert(ll(value.size()) == n);
+    ll n = sz(weight);
+    assert(sz(value) == n);
     vector<ll> dp(W + 1, 0);
     for (ll i = 1; i < n + 1; i++) {
         for (ll w = W; w >= 0; w--) {
@@ -24,35 +35,36 @@ ll knapsack(ll W, vector<ll> weight, vector<ll> value) {
     return dp[W];  // returning the maximum value of knapsack
 }
 
+void Solution() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> op(n), coins(n);
+    for (ll &B : op) cin >> B, B = operations[B];
+    for (ll &C : coins) cin >> C;
+    // DP, atmost k operations, maximise coins
+    // max(operations[i]) = 12, thus min(k, 12 * n)
+    cout << knapsack(min(k, 12 * n), op, coins) << '\n';
+}
+
 int main() {
-    vector<ll> operations(N + 1, INF);
-    operations[1] = operations[0] = 0;
+    // precalc operations required for each
+    operations[1] = 0;
     for (ll par = 1; par <= N; par++) {
         for (ll child = 1; child <= N; child++) {
             ll val = par + (par / child);
-            if (val <= N) operations[val] = min(operations[val], operations[par] + 1);
+            if (val <= N) amin(operations[val], operations[par] + 1);
         }
     }
     cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
     int tc = 1;
     cin >> tc;
     while (tc--) {
-        ll n, k;
-        cin >> n >> k;
-        vector<ll> op(n), coins(n);
-        for (ll &B : op) cin >> B, B = operations[B];
-        for (ll &C : coins) cin >> C;
-        // DP, atmost k operations, maximise coins
-        // max(operations[i]) = 12, thus min(k, 12 * n)
-        cout << knapsack(min(k, 12 * n), op, coins) << '\n';
+        Solution();
     }
     return 0;
 }
-
-/*
-1
-4 4
-1 7 5 2
-2 6 5 2
-
-*/
