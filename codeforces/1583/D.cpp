ea8827af28472866235b0b27c9e017504d6a0436
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-03 20:33:00 +0530

    Add solution for problem `D - Omkar and the Meaning of Life` Link: https://codeforces.com/contest/1583/problem/D Tags: constructive algorithms, greedy, interactive, *1800 Ref: https://codeforces.com/contest/1583/submission/145046785

--- codeforces/1583/D.cpp
+++ codeforces/1583/D.cpp
@@ -18,50 +18,40 @@ const ll MOD = 1e9 + 7;
 void Solution() {
     ll n;
     cin >> n;
-    vector<ll> query(n + 1), ans(n + 1, -1), pos(n + 1, -1);
-    // query - used to ask query
-    // ans - to output
-    // pos[i] - position of i in ans
+    vector<ll> q(n);
     auto ask = [&]() -> ll {
         cout << "? ";
-        for (ll i = 1; i <= n; ++i) cout << query[i] << ' ';
+        for (ll &x : q) cout << x << ' ';
         cout << endl;
         ll k;
         cin >> k;
-        return k;
+        return k - 1;
     };
-    // find last element
-    /*
-        I set last element to n, and then set others to 1
-        1 1 1 1 1 n, so this will return some index IF last element is 1.
-        2 2 2 2 2 n, so this will return some index IF last element is 2... thus we find last element
-
-        IF NO QUERY GIVES ANSWER, LAST ELEMENT IS n
-    */
-    for (ll i = 1; i < n; ++i) {
-        fill(all(query), i);
-        query[n] = n;
-        ll id = ask();
-        if (id == 0) continue;
-        ans[n] = i, ans[id] = n;
-        pos[i] = n, pos[n] = id;
-        break;
-    }
-    if (ans[n] == -1) ans[n] = pos[n] = n;
-    // use last element to find others
-    /*
-        last last last last last 1, gives index of 1.
-        last last last last last 2, gives index of 2...
-    */
+    vector<ll> P(n, -1);
+    // find last element first and use it to find all others
     for (ll i = 1; i <= n; ++i) {
-        if (pos[i] != -1) continue;
-        fill(all(query), ans[n]);
-        query[n] = i;
+        if (i == n) {
+            P[n - 1] = n;
+            break;
+        }
+        fill(all(q), i);
+        q[n - 1] = n;
         ll id = ask();
-        ans[id] = i, pos[i] = id;
+        if (id != -1) {
+            P[n - 1] = i;
+            break;
+        }
     }
+    for (ll i = 0; i < n && count(all(P), -1); ++i) {
+        fill(all(q), P[n - 1]);
+        q[n - 1] = i + 1;
+        ll id = ask();
+        if (id == -1) continue;
+        P[id] = i + 1;
+    }
+    dbg(P);
     cout << "! ";
-    for (ll i = 1; i <= n; ++i) cout << ans[i] << ' ';
+    for (ll &x : P) cout << x << ' ';
     cout << endl;
 }
 
