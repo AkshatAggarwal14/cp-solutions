2c265b14c240932a7a5e019eca0434b2171acd5d
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-09 19:02:00 +0530

    Add solution for problem `A - Lorenzo Von Matterhorn` Link: https://codeforces.com/contest/696/problem/A Tags: brute force, data structures, implementation, trees, *1500 Ref: https://codeforces.com/contest/696/submission/145756133

--- codeforces/696/A.cpp
+++ codeforces/696/A.cpp
@@ -16,7 +16,7 @@ constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true
 const ll MOD = 1e9 + 7;
 
 void Solution() {
-    // look at structure of tree and then observe that they always meet at some common node and that is in shortest path.
+    // 696A
     ll q, type, u, v, w;
     cin >> q;
     map<pair<ll, ll>, ll> mp;  // {{u, v}, cost}
@@ -24,18 +24,31 @@ void Solution() {
         cin >> type;
         if (type == 1) {
             cin >> u >> v >> w;
+            if (u > v) swap(u, v);  // v always larger
             while (u != v) {
-                if (u > v) swap(u, v);  // v always larger
-                mp[{v / 2, v}] += w;
-                v = v / 2;
+                if (v % 2 == 0) {
+                    mp[{v / 2, v}] += w;
+                    v = v / 2;
+                } else {
+                    mp[{(v - 1) / 2, v}] += w;
+                    v = (v - 1) / 2;
+                }
+                if (u > v) swap(u, v);
             }
+            dbg(mp);
         } else {
             cin >> u >> v;
             ll cost = 0;
+            if (u > v) swap(u, v);  // v always larger
             while (u != v) {
-                if (u > v) swap(u, v);  // v always larger
-                cost += mp[{v / 2, v}];
-                v = v / 2;
+                if (v % 2 == 0) {
+                    cost += mp[{v / 2, v}];
+                    v = v / 2;
+                } else {
+                    cost += mp[{(v - 1) / 2, v}];
+                    v = (v - 1) / 2;
+                }
+                if (u > v) swap(u, v);
             }
             cout << cost << '\n';
         }
