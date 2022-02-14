99f82673e21aae26824a9f2065cc2eaa09befe98
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-14 18:04:00 +0530

    Add solution for problem `B - Xenia and Colorful Gems` Link: https://codeforces.com/contest/1336/problem/B Tags: binary search, greedy, math, sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1336/submission/146348341

--- codeforces/1336/B.cpp
+++ codeforces/1336/B.cpp
@@ -15,20 +15,30 @@ template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
 const ll MOD = 1e9 + 7;
 
+pair<ll, ll> findClosest(const vector<ll> &x, ll target) {
+    ll mid;
+    ll lo = 0;
+    ll hi = sz(x) - 1;
+    while (hi - lo > 1) {
+        mid = ((lo + hi) / 2);
+        if (x[mid] < target) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+    return {x[lo], x[hi]};
+}
+
 // make almost equal, for each r, choose smallest g >= r, then b is lowerbound mean
 void Solution() {
-    auto closest = [](const vector<ll> &x, ll target) -> pair<ll, ll> {
-        ll lo = 0, hi = sz(x) - 1;
-        while (hi - lo > 1) {
-            ll mid = ((lo + hi) / 2);
-            if (x[mid] < target) {
-                lo = mid;
-            } else {
-                hi = mid;
-            }
-        }
-        return {x[lo], x[hi]};
-    };
+    ll nr, ng, nb;
+    cin >> nr >> ng >> nb;
+    vector<ll> rr(nr), gg(ng), bb(nb);
+    for (ll &R : rr) cin >> R;
+    for (ll &G : gg) cin >> G;
+    for (ll &B : bb) cin >> B;
+    sort(all(rr)), sort(all(gg)), sort(all(bb));
     auto val = [&](ll x, ll y, ll z) {
         return (x - y) * (x - y) + (y - z) * (y - z) + (z - x) * (z - x);
     };
@@ -37,10 +47,10 @@ void Solution() {
         for (ll i = 0; i < sz(c); ++i) c[i] *= 2;
         for (ll i = 0; i < sz(a); ++i) {
             ll R = a[i];
-            auto G = closest(b, R);
+            auto G = findClosest(b, R);
             ll G1 = G.first, G2 = G.second;
-            auto B = closest(c, R + G1);
-            auto B_ = closest(c, R + G2);
+            auto B = findClosest(c, R + G1);
+            auto B_ = findClosest(c, R + G2);
             ll B1 = B.first, B2 = B.second, B3 = B_.first, B4 = B_.second;
             amin(res, val(R, G1, B1 / 2));
             amin(res, val(R, G2, B1 / 2));
@@ -53,15 +63,7 @@ void Solution() {
         }
         return res;
     };
-
-    ll nr, ng, nb;
-    cin >> nr >> ng >> nb;
-    vector<ll> rr(nr), gg(ng), bb(nb);
-    for (ll &R : rr) cin >> R;
-    for (ll &G : gg) cin >> G;
-    for (ll &B : bb) cin >> B;
-    sort(all(rr)), sort(all(gg)), sort(all(bb));
-    cout << min({solve(rr, gg, bb), solve(gg, bb, rr), solve(bb, rr, gg)}) << '\n';
+    cout << min({solve(rr, gg, bb), solve(gg, bb, rr), solve(rr, bb, gg)}) << '\n';
 }
 
 int main() {
