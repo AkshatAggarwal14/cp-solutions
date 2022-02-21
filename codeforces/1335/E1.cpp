3fcf1d6bf851240303512e55a6c7dd353cf7b13b
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-21 12:57:00 +0530

    Add solution for problem `E1 - Three Blocks Palindrome (easy version)` Link: https://codeforces.com/contest/1335/problem/E1 Tags: binary search, brute force, data structures, dp, two pointers, *1700 Ref: https://codeforces.com/contest/1335/submission/147149407

--- codeforces/1335/E1.cpp
+++ codeforces/1335/E1.cpp
@@ -6,45 +6,44 @@
 using namespace std;
 #define dbg(...)
 #endif
-constexpr auto sz = [](const auto &container) -> int { return int(container.size()); };
+using ll = long long;
+constexpr auto sz = [](const auto &container) -> ll { return ll(container.size()); };
 #define all(x) (x).begin(), (x).end()
 template <class T, class U = T>
 constexpr bool amin(T &a, U &&b) { return b < a && (a = std::forward<U>(b), true); }
 template <class T, class U = T>
 constexpr bool amax(T &a, U &&b) { return a < b && (a = std::forward<U>(b), true); }
-const int MOD = 1e9 + 7;
-const int N = 200;
+const ll MOD = 1e9 + 7;
+const ll N = 26;
 
 void Solution() {
-    // use int to avoid MLE
-    // For some a_i, find max number of a_j's present between when you take 1 a_i from left and 1 from right
-    int n;
+    ll n;
     cin >> n;
-    vector<int> a(n);
-    for (int &A : a) cin >> A;
-    vector<vector<int>> pos(N + 1), pref(N + 1, vector<int>(n, 0));
-    for (int i = 0; i < n; ++i) {
+    vector<ll> a(n);
+    for (ll &A : a) cin >> A;
+    vector<vector<ll>> pos(N + 1), pref(N + 1, vector<ll>(n, 0));
+    for (ll i = 0; i < n; ++i) {
         pos[a[i]].push_back(i);
         pref[a[i]][i] = 1;
     }
-    for (int i = 1; i <= N; ++i) {
-        for (int j = 1; j < n; ++j) {
+    for (ll i = 1; i <= N; ++i) {
+        for (ll j = 1; j < n; ++j) {
             pref[i][j] += pref[i][j - 1];
         }
     }
-    auto count_of = [&](int value, int L, int R) -> int {
+    auto count_of = [&](ll value, ll L, ll R) -> ll {
         // between L and R
         L++, R--;
         if (L > R) return 0LL;
         return pref[value][R] - pref[value][L - 1];
     };
-    int best = 1;
-    for (int A = 1; A <= N; ++A) {
-        int l = 0, r = sz(pos[A]) - 1;
-        int ans = 1;
+    ll best = 1;
+    for (ll A = 1; A <= N; ++A) {
+        ll l = 0, r = sz(pos[A]) - 1;
+        ll ans = 1;
         while (l < r) {
-            for (int B = 1; B <= N; ++B) {
-                int y = count_of(B, pos[A][l], pos[A][r]);
+            for (ll B = 1; B <= N; ++B) {
+                ll y = count_of(B, pos[A][l], pos[A][r]);
                 if (y > 0 || (B == A)) amax(ans, 2 * (l + 1) + y);
             }
             ++l, --r;
