eaa95a6c3dfe97be417e47659b3c5abd6d756863
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-23 02:41:00 +0530

    Add solution for problem `D - Dasha and Very Difficult Problem` Link: https://codeforces.com/contest/761/problem/D Tags: binary search, brute force, constructive algorithms, greedy, sortings, *1700 Ref: https://codeforces.com/contest/761/submission/147371988

--- codeforces/761/D.cpp
+++ codeforces/761/D.cpp
@@ -11,32 +11,42 @@ auto sz = [](const auto &container) -> ll { return ll(container.size()); };
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 
-#define ff first
-#define ss second
 using node = pair<ll, pair<ll, ll>>;
+
 void Solution() {
     ll n, l, r;
     cin >> n >> l >> r;
-    vector<node> a(n);
-    for (ll i = 0; i < n; ++i) cin >> a[i].ff;
-    for (ll i = 0; i < n; ++i) cin >> a[i].ss.ff, a[i].ss.ss = i;
-    sort(all(a), [](const node &A, const node &B) {
-        return A.ss.ff < B.ss.ff;
-    });
-    ll prev = -INF;
-    vector<pair<ll, ll>> b(n);
-    for (ll i = 0; i < n; ++i) b[i].ss = a[i].ss.ss;
+    vector<node> v(n);
+    for (ll i = 0; i < n; ++i) cin >> v[i].first;
     for (ll i = 0; i < n; ++i) {
-        // min C in range, [l - a[i], r - a[i]] such that C > prev as c[i] = b[i] - a[i]
-        // C = max(l - a[i].ff, prev + 1)
-        ll C = max(l - a[i].ff, prev + 1);
-        b[i].ff = C + a[i].ff;
-        if (b[i].ff > r) return void(cout << "-1\n");
+        cin >> v[i].second.first, --v[i].second.first;
+        v[i].second.second = i;
+    }
+    sort(all(v), [](const node &A, const node &B) { return A.second.first < B.second.first; });
+    ll prev = -INF;
+    vector<pair<ll, ll>> b(n, {-1, -1});
+    for (ll i = 0; i < n; ++i) {
+        b[i].second = v[i].second.second;
+        ll &B = b[i].first, &A = v[i].first;
+        // BS on C, find min C in range, [l - a[i], r - a[i]] such that C > prev as c[i] = b[i] - a[i]
+        ll C = -INF;
+        ll L = l - A, R = r - A;
+        --L, ++R;
+        while (R > L + 1) {
+            ll M = (L + R) / 2;
+            if (M > prev) {
+                R = M;
+                C = M;
+            } else {
+                L = M;
+            }
+        }
+        if (C == -INF) return void(cout << "-1\n");
+        B = C + A;
         prev = C;
     }
-    sort(all(b), [](const pair<ll, ll> &A, const pair<ll, ll> &B) {
-        return A.ss < B.ss;
-    });
+    dbg(v, b);
+    sort(all(b), [](const pair<ll, ll> &A, const pair<ll, ll> &B) { return A.second < B.second; });
     for (auto &[x, y] : b) cout << x << ' ';
     cout << '\n';
 }
