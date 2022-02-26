fe8a86993be2a89dff0be148319033ad44fc7fb6
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-27 00:33:00 +0530

    Add solution for problem `A - Spy Detected!` Link: https://codeforces.com/contest/1645/problem/A Tags: *special problem, implementation Ref: https://codeforces.com/contest/1645/submission/147802444

--- codeforces/1645/A.kt
+++ codeforces/1645/A.kt
@@ -1,6 +1,7 @@
 #![allow(dead_code, unused_macros, unused_imports)]
 import java.io.BufferedReader
-import kotlin.math.*
+
+//import kotlin.math.*
 
 fun main() {
     val br = System.`in`.bufferedReader()
@@ -18,13 +19,13 @@ fun main() {
         }
         var ans = -1
         for ((x, y) in freq) {
-            if (y == 1) {
+            if (y.equals(1)) {
                 ans = x
             }
         }
         var idx = -1
-        for (i in 0..n - 1) {
-            if (a[i] == ans)
+        for (i in 0 until n) {
+            if (a[i].equals(ans))
                 idx = i + 1
         }
         sb.appendLine(idx)
@@ -37,10 +38,14 @@ private const val ZERO_INT = '0'.toInt()
 private const val NL_INT = '\n'.toInt()
 
 private fun BufferedReader.readInt(): Int {
-    var ret: Int = read()
-    while (ret <= SPACE_INT) ret = read()
+    var ret = read()
+    while (ret <= SPACE_INT) {
+        ret = read()
+    }
     val neg = ret == '-'.toInt()
-    if (neg) ret = read()
+    if (neg) {
+        ret = read()
+    }
     ret -= ZERO_INT
     var read = read()
     while (read >= ZERO_INT) {
@@ -48,11 +53,14 @@ private fun BufferedReader.readInt(): Int {
         ret += read - ZERO_INT
         read = read()
     }
+
     while (read <= SPACE_INT && read != -1 && read != NL_INT) {
         mark(1)
         read = read()
     }
-    if (read > SPACE_INT) reset()
+    if (read > SPACE_INT) {
+        reset()
+    }
     return if (neg) -ret else ret
 }
 
