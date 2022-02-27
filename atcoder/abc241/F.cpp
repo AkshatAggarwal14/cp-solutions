6a36b0d137dabb6ec176ca83b2747d5fd1aae9e9
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-02-27 13:18:00 +0530

    Add solution for problem `F - Skate` Link: https://atcoder.jp/contests/abc241/tasks/abc241_f Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc241/submissions/29734358

--- atcoder/abc241/F.cpp
+++ atcoder/abc241/F.cpp
@@ -1,88 +1,134 @@
-#ifdef LOCAL
-#include "Akshat.hpp"
-#else
-#include "bits/stdc++.h"
-using namespace std;
-#define dbg(...)
-#endif
-using ll = int;
-auto sz = [](const auto &container) -> ll { return ll(container.size()); };
-#define all(x) (x).begin(), (x).end()
-const ll MOD = 1e9 + 7;
+#include <bits/stdc++.h>
 
-void Solution() {
-    ll h, w, n;
-    cin >> h >> w >> n;
-    ll sx, sy, gx, gy;
-    cin >> sx >> sy >> gx >> gy;
-    map<ll, vector<ll>> X, Y;
-    for (ll i = 0, x, y; i < n; ++i) {
-        cin >> x >> y;
-        X[x].push_back(y);
-        Y[y].push_back(x);
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+#define pb push_back
+#define ff first
+#define ss second
+const int N = 3e5 + 7;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 3.14159265358979323846;
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    op_set;
+
+int higher(vector<int>& ar, int ex) {
+    if (ar.back() < ex)
+        return -1;
+    int low = 0;
+    int high = ar.size() - 1;
+    int ans = high;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (ar[mid] > ex) {
+            ans = std::min(ans, mid);
+            high = mid - 1;
+        } else
+            low = mid + 1;
     }
-    auto req_pair = [&](const vector<ll> &a, const ll &value) -> pair<ll, ll> {
-        ll L = 0, R = sz(a) - 1;
-        ll ans_L = INT_MAX, ans_R = INT_MIN;
-        --L, ++R;
-        while (R > L + 1) {
-            ll M = (L + R) / 2;
-            if (a[M] < value) {
-                L = M;
-                ans_L = a[L];
-            } else {
-                R = M;
-                ans_R = a[R];
-            }
-        }
-        return {ans_L, ans_R};
-    };
-    for (auto &[_, v] : X) sort(all(v));
-    for (auto &[_, v] : Y) sort(all(v));
-    map<pair<ll, ll>, ll> d;
-    queue<pair<ll, ll>> bfs;
-    bfs.emplace(sx, sy);
-    d[{sx, sy}] = 0;
-    while (!bfs.empty()) {
-        auto [x, y] = bfs.front();
-        bfs.pop();
-        if (X.count(x)) {
-            auto [L, R] = req_pair(X[x], y);
-            if (L != INT_MAX && !d.count({x, L + 1})) {
-                bfs.emplace(x, L + 1);
-                d[{x, L + 1}] = d[{x, y}] + 1;
-            }
-            if (R != INT_MIN && !d.count({x, R - 1})) {
-                bfs.emplace(x, R - 1);
-                d[{x, R - 1}] = d[{x, y}] + 1;
-            }
-        }
-        if (Y.count(y)) {
-            auto [L, R] = req_pair(Y[y], x);
-            if (L != INT_MAX && !d.count({L + 1, y})) {
-                bfs.emplace(L + 1, y);
-                d[{L + 1, y}] = d[{x, y}] + 1;
-            }
-            if (R != INT_MIN && !d.count({R - 1, y})) {
-                bfs.emplace(R - 1, y);
-                d[{R - 1, y}] = d[{x, y}] + 1;
-            }
-        }
-    }
-    cout << (d.count({gx, gy}) ? d[{gx, gy}] : -1) << '\n';
+    return ar[ans];
 }
 
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
-    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
-#endif
-    cout << fixed << setprecision(12);
-    int tc = 1;
-    // cin >> tc;
-    while (tc--) {
-        Solution();
+int lower(vector<int>& ar, int ex) {
+    if (ar[0] > ex)
+        return -1;
+    int low = 0;
+    int high = ar.size() - 1;
+    int ans = low;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (ar[mid] < ex) {
+            ans = std::max(ans, mid);
+            low = mid + 1;
+        } else
+            high = mid - 1;
     }
-    return 0;
+    return ar[ans];
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int h, w, n;
+    cin >> h >> w >> n;
+
+    int startx, starty;
+    cin >> startx >> starty;
+
+    int endx, endy;
+    cin >> endx >> endy;
+
+    map<int, vector<int>> row, col;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        row[a].pb(b);
+        col[b].pb(a);
+    }
+
+    for (auto& ele : row)
+        sort(ele.ss.begin(), ele.ss.end());
+
+    for (auto& ele : col)
+        sort(ele.ss.begin(), ele.ss.end());
+
+    queue<pair<int, int>> bfs;
+    map<pair<int, int>, int> dist;
+    bfs.push({startx, starty});
+
+    dist[{startx, starty}] = 0;
+    while (!bfs.empty()) {
+        auto node = bfs.front();
+        bfs.pop();
+
+        int x = node.ff;
+        int y = node.ss;
+
+        // cout << x << " " << y << " -> " << dist[{x,y}] << endl;
+        if (row.find(x) != row.end()) {
+            int k = higher(row[x], y);
+            if (k != -1) {
+                if (dist.find({x, k - 1}) == dist.end()) {
+                    dist[{x, k - 1}] = dist[{x, y}] + 1;
+                    bfs.push({x, k - 1});
+                }
+            }
+
+            k = lower(row[x], y);
+            if (k != -1) {
+                if (dist.find({x, k + 1}) == dist.end()) {
+                    dist[{x, k + 1}] = dist[{x, y}] + 1;
+                    bfs.push({x, k + 1});
+                }
+            }
+        }
+
+        if (col.find(y) != col.end()) {
+            int k = higher(col[y], x);
+            if (k != -1) {
+                if (dist.find({k - 1, y}) == dist.end()) {
+                    dist[{k - 1, y}] = dist[{x, y}] + 1;
+                    bfs.push({k - 1, y});
+                }
+            }
+
+            k = lower(col[y], x);
+            if (k != -1) {
+                if (dist.find({k + 1, y}) == dist.end()) {
+                    dist[{k + 1, y}] = dist[{x, y}] + 1;
+                    bfs.push({k + 1, y});
+                }
+            }
+        }
+    }
+
+    if (dist.find({endx, endy}) == dist.end())
+        cout << -1 << "\n";
+    else
+        cout << dist[{endx, endy}] << "\n";
 }
