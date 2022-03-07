c20a07e24a37fc59b4de28d513106cb3edb9a56d
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-03-07 10:59:00 +0530

    Add solution for problem `C - Add One` Link: https://codeforces.com/contest/1513/problem/C Tags: dp, matrices, *1600 Ref: https://codeforces.com/contest/1513/submission/148663209

--- codeforces/1513/C.cpp
+++ codeforces/1513/C.cpp
@@ -1,27 +1,32 @@
+#ifdef LOCAL
+#include "Akshat.hpp"
+#else
 #include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#endif
 
 using ll = long long;
+#define int ll
 auto sz = [](const auto &container) -> ll { return ll(container.size()); };
 #define all(x) (x).begin(), (x).end()
 const ll INF = 1e18;
 const ll M = 2e5 + 5;
 
-// dp[dig][x][i] is count of digit `i` after `x` moves on digit `dig`
-vector<vector<vector<ll>>> dp;
-const ll MOD = 1e9 + 7;
+vector<vector<vector<int>>> real_count(10, vector<vector<int>>(M + 1, vector<int>(10, 0)));
+const int MOD = 1e9 + 7;
 void Solution() {
-    ll n, m;
+    int n, m;
     cin >> n >> m;
-    vector<ll> cnt_digits(10, 0);
+    vector<int> cnt(10, 0);
     while (n) {
-        ++cnt_digits[n % 10];
+        ++cnt[n % 10];
         n /= 10;
     }
-    vector<ll> ans(10, 0);
-    for (ll i = 0; i < 10; ++i) {
-        for (ll j = 0; j < 10; ++j) {
-            ans[j] = (ans[j] + (cnt_digits[i] % MOD) * (dp[i][m][j] % MOD)) % MOD;
+    vector<int> ans(10, 0);
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            ans[j] = (ans[j] + (cnt[i] % MOD) * (real_count[i][m][j] % MOD)) % MOD;
         }
     }
     ll res = 0;
@@ -31,22 +36,19 @@ void Solution() {
 
 int32_t main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    vector<ll> temp(10, 0);
-    dp.resize(10, vector<vector<ll>>(M + 1, vector<ll>(10, 0)));
+    vector<int> temp(10, 0);
     // preprocess answer for 1, 2, 3, 4, 5,.. 9 for all 1 <= m <= 2e5, then use it to calculate total answer per digit in input
-    for (ll digit = 0; digit <= 9; ++digit) {
-        dp[digit][0][digit] = 1;  // on 0 moves, only the digit itself is set
-        for (ll move_number = 1; move_number <= M; ++move_number) {
-            for (ll i = 0; i <= 9; ++i) temp[i] = 0;
-            for (ll i = 0; i <= 9; ++i) {
-                ll next = i + 1;
-                while (next) {
-                    temp[next % 10] += dp[digit][move_number - 1][i] % MOD;
-                    temp[next % 10] %= MOD;
-                    next /= 10;
+    for (int digit = 0; digit <= 9; ++digit) {
+        real_count[digit][0][digit] = 1;
+        for (int mm = 1; mm <= M; ++mm) {
+            for (int i = 0; i <= 9; ++i) temp[i] = 0;
+            for (int i = 0; i <= 9; ++i) {
+                for (char &c : to_string(i + 1)) {
+                    temp[c - '0'] =
+                        (temp[c - '0'] + real_count[digit][mm - 1][i]) % MOD;
                 }
             }
-            swap(temp, dp[digit][move_number]);  // update the next state from previous
+            swap(temp, real_count[digit][mm]);
         }
     }
 
