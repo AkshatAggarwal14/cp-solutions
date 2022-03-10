97302011ed6953bae55ae6c95718dd27c178286b
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-03-10 16:03:00 +0530

    Add solution for problem `D - Yet Another Monster Killing Problem` Link: https://codeforces.com/contest/1257/problem/D Tags: binary search, data structures, dp, greedy, sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1257/submission/149069274

--- codeforces/1257/D.cpp
+++ codeforces/1257/D.cpp
@@ -55,13 +55,12 @@ void Solution() {
     for (ll &A : a) cin >> A;
     ll n_heroes;
     cin >> n_heroes;
-    vector<ll> max_power(n, -INF);  // find max power for given endurance
-    // if hero can do 5 monsters, with power 100, he can do 4 too
+    vector<ll> max_p(n, -INF);  // find max power for given endurance
     for (ll i = 0, p, si; i < n_heroes; ++i) {
         cin >> p >> si, --si;
-        max_power[si] = max(max_power[si], p);
+        max_p[si] = max(max_p[si], p);
     }
-    for (ll i = n - 2; i >= 0; --i) max_power[i] = max(max_power[i], max_power[i + 1]);
+    for (ll i = n - 2; i >= 0; --i) max_p[i] = max(max_p[i], max_p[i + 1]);
     Sparse_Table<ll> st(
         a,
         [](const ll &A, const ll &B) -> ll { return max(A, B); },
@@ -74,17 +73,15 @@ void Solution() {
         --L, ++R;
         while (R > L + 1) {
             int M = (L + R) / 2;
-            // if max power of given length is >= max of array
-            // binary search for how far can i go (longest endurance possible) if the hero selected can fight the monster with maximum power in this range
-            if (max_power[M - index] >= st.query(index, M)) {
+            if (max_p[M - index] >= st.query(index, M)) {
                 L = M;
             } else {
                 R = M;
             }
         }
-        if (index == L + 1) return void(cout << "-1\n");  // if none found
-        index = L + 1;                                    // if found move to next segment
-        ++ans;                                            // how many segments?
+        if (index == L + 1) return void(cout << "-1\n");
+        index = L + 1;
+        ++ans;
     }
     cout << ans << '\n';
 }
