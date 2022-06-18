624102ae6af4eaa1735c1707fca5787a7e223aab
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-06-18 20:09:00 +0530

    Add solution for problem `F - Cumulative Cumulative Cumulative Sum` Link: https://atcoder.jp/contests/abc256/tasks/abc256_f Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc256/submissions/32574957

--- atcoder/abc256/F.cpp
+++ atcoder/abc256/F.cpp
@@ -1,298 +1,169 @@
-/**
- *    author:  tourist
- *    created: 18.06.2022 16:12:24
- **/
-#include <bits/stdc++.h>
-
+#ifdef LOCAL
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#endif
+using ll = long long;
+auto sz = [](const auto &container) { return int(container.size()); };
+#define all(x) begin(x), end(x)
 
-template <typename T>
-T inverse(T a, T m) {
-    T u = 0, v = 1;
-    while (a != 0) {
-        T t = m / a;
-        m -= t * a;
-        swap(a, m);
-        u -= t * v;
-        swap(u, v);
-    }
-    assert(m == 1);
+const ll INF = 1e18;
+const ll N = 1e5 + 5;
+const ll MOD = 998244353;
+
+ll M(ll x) { return ((x % MOD) + MOD) % MOD; }
+ll add(ll x, ll y) { return (M(x) + M(y)) % MOD; }
+ll mul(ll x, ll y) { return (M(x) * M(y)) % MOD; }
+ll modpow(ll x, ll y, ll m) {
+    if (y == 0) return 1 % m;
+    ll u = modpow(x, y / 2, m);
+    u = (u * u) % m;
+    if (y % 2 == 1) u = (u * x) % m;
     return u;
 }
+ll inverse(ll a, ll m = MOD) { return modpow(a, m - 2, m); }
 
-template <typename T>
-class Modular {
+template <class T, class op = function<T(const T &, const T &)>, class id = function<T()>>
+class SegTree {
    public:
-    using Type = typename decay<decltype(T::value)>::type;
-
-    constexpr Modular() : value() {}
-    template <typename U>
-    Modular(const U& x) {
-        value = normalize(x);
+    SegTree() = default;
+    SegTree(int n, op operation_, id identity_)
+        : SegTree(vector<T>(n, identity_()), operation_, identity_) {}
+    int ceil_pow2(int n) {
+        int x = 0;
+        while ((1U << x) < (unsigned int)(n)) x++;
+        return x;
+    }
+    SegTree(const vector<T> &v, op operation_, id identity_)
+        : operation(operation_), initialize(identity_), _n(int(v.size())) {
+        height = ceil_pow2(_n);
+        size = (1 << height);
+        tree.resize(2 * size, initialize());
+        for (int i = 0; i < _n; i++) tree[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            calc(i);
+        }
     }
 
-    template <typename U>
-    static Type normalize(const U& x) {
-        Type v;
-        if (-mod() <= x && x < mod())
-            v = static_cast<Type>(x);
-        else
-            v = static_cast<Type>(x % mod());
-        if (v < 0) v += mod();
-        return v;
+    T _query(int node, int node_lo, int node_hi, int q_lo, int q_hi) {
+        // if range is completely inside [q_lo, q_hi], then just return its ans
+        if (q_lo <= node_lo && node_hi <= q_hi)
+            return tree[node];
+        if (node_hi < q_lo || q_hi < node_lo)
+            return initialize();  // if disjoint ignore
+        int last_in_left = (node_lo + node_hi) / 2;
+        return operation(_query(2 * node, node_lo, last_in_left, q_lo, q_hi),
+                         _query(2 * node + 1, last_in_left + 1, node_hi, q_lo, q_hi));
     }
 
-    const Type& operator()() const { return value; }
-    template <typename U>
-    explicit operator U() const { return static_cast<U>(value); }
-    constexpr static Type mod() { return T::value; }
+    void _update(int node, int node_lo, int node_hi, int q_lo, int q_hi, T value) {
+        // happens only once when leaf [id, id]
+        if (q_lo <= node_lo && node_hi <= q_hi) {
+            tree[node] = value;
+            return;
+        }
+        // in disjoint just return
+        if (node_hi < q_lo || q_hi < node_lo) return;
+        int last_in_left = (node_lo + node_hi) / 2;
+        _update(2 * node, node_lo, last_in_left, q_lo, q_hi, value);
+        _update(2 * node + 1, last_in_left + 1, node_hi, q_lo, q_hi, value);
 
-    Modular& operator+=(const Modular& other) {
-        if ((value += other.value) >= mod()) value -= mod();
-        return *this;
-    }
-    Modular& operator-=(const Modular& other) {
-        if ((value -= other.value) < 0) value += mod();
-        return *this;
-    }
-    template <typename U>
-    Modular& operator+=(const U& other) { return *this += Modular(other); }
-    template <typename U>
-    Modular& operator-=(const U& other) { return *this -= Modular(other); }
-    Modular& operator++() { return *this += 1; }
-    Modular& operator--() { return *this -= 1; }
-    Modular operator++(int) {
-        Modular result(*this);
-        *this += 1;
-        return result;
-    }
-    Modular operator--(int) {
-        Modular result(*this);
-        *this -= 1;
-        return result;
-    }
-    Modular operator-() const { return Modular(-value); }
-
-    template <typename U = T>
-    typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
-#ifdef _WIN32
-        uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
-        uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
-        asm(
-            "divl %4; \n\t"
-            : "=a"(d), "=d"(m)
-            : "d"(xh), "a"(xl), "r"(mod()));
-        value = m;
-#else
-        value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
-#endif
-        return *this;
-    }
-    template <typename U = T>
-    typename enable_if<is_same<typename Modular<U>::Type, long long>::value, Modular>::type& operator*=(const Modular& rhs) {
-        long long q = static_cast<long long>(static_cast<long double>(value) * rhs.value / mod());
-        value = normalize(value * rhs.value - q * mod());
-        return *this;
-    }
-    template <typename U = T>
-    typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
-        value = normalize(value * rhs.value);
-        return *this;
+        // after updating now set, Post Call Area
+        calc(node);
     }
 
-    Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+    T _kth_order(int node, int node_lo, int node_hi, T k) {
+        if (node_lo == node_hi) return node_lo;
+        int last_in_left = (node_lo + node_hi) >> 1;
+        if (tree[2 * node] >= k) return _kth_order(2 * node, node_lo, last_in_left, k);
+        return _kth_order(2 * node + 1, last_in_left + 1, node_hi, k - tree[2 * node]);
+    }
 
-    friend const Type& abs(const Modular& x) { return x.value; }
-
-    template <typename U>
-    friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
-
-    template <typename U>
-    friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
-
-    template <typename V, typename U>
-    friend V& operator>>(V& stream, Modular<U>& number);
+    T all_query() { return tree[1]; }
+    T query(int p) {
+        assert(0 <= p && p < _n);
+        return tree[p + size];
+    }
+    T query(int l, int r) {
+        assert(0 <= l && l <= r && r < _n);
+        return _query(1, 0, size - 1, l, r);
+    }
+    void update(int p, T x) {
+        assert(0 <= p && p < _n);
+        _update(1, 0, size - 1, p, p, x);
+    }
+    T kth_order(T k) {
+        assert(k <= tree[1]);
+        return _kth_order(1, 0, size - 1, k);
+    }
 
    private:
-    Type value;
+    vector<T> tree;
+    void calc(int k) { tree[k] = operation(tree[2 * k], tree[2 * k + 1]); }
+    op operation;
+    id initialize;
+    int _n, size, height;
 };
 
-template <typename T>
-bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
-template <typename T, typename U>
-bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
-template <typename T, typename U>
-bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
-
-template <typename T>
-bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
-template <typename T, typename U>
-bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
-template <typename T, typename U>
-bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
-
-template <typename T>
-bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
-
-template <typename T>
-Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
-template <typename T, typename U>
-Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
-template <typename T, typename U>
-Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
-
-template <typename T>
-Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
-template <typename T, typename U>
-Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
-template <typename T, typename U>
-Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
-
-template <typename T>
-Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
-template <typename T, typename U>
-Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
-template <typename T, typename U>
-Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
-
-template <typename T>
-Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
-template <typename T, typename U>
-Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
-template <typename T, typename U>
-Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
-
-template <typename T, typename U>
-Modular<T> power(const Modular<T>& a, const U& b) {
-    assert(b >= 0);
-    Modular<T> x = a, res = 1;
-    U p = b;
-    while (p > 0) {
-        if (p & 1) res *= x;
-        x *= x;
-        p >>= 1;
-    }
-    return res;
-}
-
-template <typename T>
-bool IsZero(const Modular<T>& number) {
-    return number() == 0;
-}
-
-template <typename T>
-string to_string(const Modular<T>& number) {
-    return to_string(number());
-}
-
-// U == std::ostream? but done this way because of fastoutput
-template <typename U, typename T>
-U& operator<<(U& stream, const Modular<T>& number) {
-    return stream << number();
-}
-
-// U == std::istream? but done this way because of fastinput
-template <typename U, typename T>
-U& operator>>(U& stream, Modular<T>& number) {
-    typename common_type<typename Modular<T>::Type, long long>::type x;
-    stream >> x;
-    number.value = Modular<T>::normalize(x);
-    return stream;
-}
-
-/*
-using ModType = int;
-
-struct VarMod { static ModType value; };
-ModType VarMod::value;
-ModType& md = VarMod::value;
-using Mint = Modular<VarMod>;
-*/
-
-constexpr int md = 998244353;
-using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
-
-/*vector<Mint> fact(1, 1);
-vector<Mint> inv_fact(1, 1);
-
-Mint C(int n, int k) {
-  if (k < 0 || k > n) {
-    return 0;
-  }
-  while ((int) fact.size() < n + 1) {
-    fact.push_back(fact.back() * (int) fact.size());
-    inv_fact.push_back(1 / fact.back());
-  }
-  return fact[n] * inv_fact[k] * inv_fact[n - k];
-}*/
-
-template <typename T>
-class fenwick {
-   public:
-    vector<T> fenw;
-    int n;
-
-    fenwick(int _n) : n(_n) {
-        fenw.resize(n);
-    }
-
-    void modify(int x, T v) {
-        while (x < n) {
-            fenw[x] += v;
-            x |= (x + 1);
-        }
-    }
-
-    T get(int x) {
-        T v{};
-        while (x >= 0) {
-            v += fenw[x];
-            x = (x & (x + 1)) - 1;
-        }
-        return v;
-    }
-};
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+void test() {
+    // for x >= i, a_i contributes to b_x, 1 time
+    // a_i contributed to c_x, (x - i + 1) times
+    // a_i contributed to d_x, (x - i + 1) * (x - i + 2) / 2 times n*(n+1)/2
+    // d_i = Sigma (a_i * ((x - i + 1) * (x - i + 2) / 2)) from i = 0 to x
+    // expand d_i and break into a_i, i * a_i, i * i * a_i
+    // so maintain one segtree each for a_i, i * a_i, i * i * a_i
     int n, q;
     cin >> n >> q;
-    vector<int> a(n);
+    vector<ll> a(n);
+    SegTree<ll> A(
+        n, [](const ll &a_, const ll &b_) { return add(a_, b_); }, []() { return 0LL; });
+    auto iA = A, iiA = A;
+    ll two_inv = inverse(2);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-    }
-    fenwick<Mint> f0(n);
-    fenwick<Mint> f1(n);
-    fenwick<Mint> f2(n);
-    for (int i = 0; i < n; i++) {
-        f0.modify(i, Mint(a[i]));
-        f1.modify(i, Mint(a[i]) * i);
-        f2.modify(i, Mint(a[i]) * i * i);
+        ll val = a[i];
+        A.update(i, val);
+        val = mul(i, val);
+        iA.update(i, val);
+        val = mul(i, val);
+        iiA.update(i, val);
     }
     while (q--) {
-        int op;
-        cin >> op;
-        if (op == 1) {
-            int x, v;
-            cin >> x >> v;
-            --x;
-            Mint delta = v - a[x];
-            f0.modify(x, Mint(delta));
-            f1.modify(x, Mint(delta) * x);
-            f2.modify(x, Mint(delta) * x * x);
-            a[x] = v;
+        int type, x;
+        ll v;
+        cin >> type >> x, --x;
+        if (type == 1) {
+            cin >> v;
+            A.update(x, v);
+            v = mul(x, v);
+            iA.update(x, v);
+            v = mul(x, v);
+            iiA.update(x, v);
+
+        } else {
+            ll sum = 0;
+            sum = add(sum, iiA.query(0, x));
+            sum = add(sum, mul(mul(-1, 2 * x + 3), iA.query(0, x)));
+            sum = add(sum, mul(mul(x + 1, x + 2), A.query(0, x)));
+            sum = mul(sum, two_inv);
+            cout << sum << '\n';
         }
-        if (op == 2) {
-            int x;
-            cin >> x;
-            --x;
-            Mint s0 = f0.get(x);
-            Mint s1 = f1.get(x);
-            Mint s2 = f2.get(x);
-            Mint res = s2 - s1 * ((x + 1) + (x + 2)) + s0 * (x + 1) * (x + 2);
-            cout << res / 2 << '\n';
-        }
+    }
+}
+
+int32_t main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
+    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    for (int tt = 1; tt <= tc; ++tt) {
+        test();
     }
     return 0;
 }
