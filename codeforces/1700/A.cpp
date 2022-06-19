67f6e0220b6de3e346aefb6c26d7cf20b1f827af
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-06-19 15:01:00 +0530

    Add solution for problem `A - Optimal Path` Link: https://codeforces.com/contest/1700/problem/A Tags: dp, greedy, greedy, math, math, *800 Ref: https://codeforces.com/contest/1700/submission/161181681

--- codeforces/1700/A.cpp
+++ codeforces/1700/A.cpp
@@ -16,8 +16,11 @@ const ll MOD = 1e9 + 7;  // 998244353
 void test() {
     ll n, m;
     cin >> n >> m;
-    // (1, 1) -> (1, m) -> (n, m)
-    cout << m * (m - 1) / 2 + m * (n * (n + 1)) / 2 << '\n';
+    auto get = [&](ll i, ll j) { return (i - 1) * m + j; };
+    auto AP = [&](ll a, ll l, ll n_) { return (n_ * (a + l)) / 2; };
+    ll ans1 = AP(get(1, 1), get(n, 1), n) + AP(get(n, 1), get(n, m), m) - get(n, 1);
+    ll ans2 = AP(get(1, 1), get(1, m), m) + AP(get(1, m), get(n, m), n) - get(1, m);
+    cout << min(ans1, ans2) << '\n';
 }
 
 int32_t main() {
