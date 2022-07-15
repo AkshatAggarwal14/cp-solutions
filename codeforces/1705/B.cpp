b776d11d2f8f3ab68c882758eefa343b54c9d795
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-15 19:48:00 +0530

    Add solution for problem `B - Mark the Dust Sweeper` Link: https://codeforces.com/contest/1705/problem/B Tags: constructive algorithms, greedy, implementation Ref: https://codeforces.com/contest/1705/submission/164300125

--- codeforces/1705/B.cpp
+++ codeforces/1705/B.cpp
@@ -1,48 +1,43 @@
-#ifdef LOCAL
-#include "Akshat.hpp"
-#else
 #include "bits/stdc++.h"
 using namespace std;
-#define dbg(...)
-#endif
 using ll = long long;
-auto sz = [](const auto &container) { return int(container.size()); };
-#define all(x) begin(x), end(x)
-
-const ll INF = 1e18;
-const ll N = 1e5 + 5;
-const ll MOD = 1e9 + 7;  // 998244353
-
+#define int ll
 void test() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto &A : a) cin >> A;
-    reverse(a.begin(), a.end());
-    while (int(a.size()) > 1 && a.back() == 0) a.pop_back();
-    reverse(a.begin(), a.end());
-    ll ans = 0;
-    n = int(a.size());
-    for (int i = 0; i < n - 1; ++i) {
-        if (a[i])
-            ans += a[i];
-        else
-            ++ans;
-    }
-    cout << ans << '\n';
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(auto &A: a) cin >> A;
+	a.pop_back();
+	reverse(a.begin(), a.end());
+	while(!a.empty() && a.back() == 0) a.pop_back();
+	reverse(a.begin(), a.end());
+	set<int> zeroes;
+	n = int(a.size());
+	for(int i = 0; i < n; ++i) {
+		if(a[i] == 0){ 
+			zeroes.insert(i);
+			// cerr << i << '\n';
+		}
+	}
+	int ans = 0;
+	for(int i = 0; i < n; ++i) {
+		while(a[i] > 0 && !zeroes.empty()) {
+			int ii = *zeroes.begin();
+			a[ii] = 1;
+			zeroes.erase(ii);
+			++ans;
+			--a[i];
+		}
+	}
+	for(int i = 0; i < n; ++i) ans += a[i];
+	cout << ans << '\n';
 }
 
 int32_t main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-#ifdef LOCAL
-    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
-    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
-#endif
-    cout << fixed << setprecision(12);
-    int tc = 1;
-    cin >> tc;
-    for (int tt = 1; tt <= tc; ++tt) {
-        test();
-    }
-    return 0;
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int tc = 1;
+	cin >> tc;
+	while(tc--) {
+		test();
+	}
 }
