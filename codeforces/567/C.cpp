09466bbb4c77a9cfd985a842c3d88455b4fccaa1
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-17 01:07:00 +0530

    Add solution for problem `C - Geometric Progression` Link: https://codeforces.com/contest/567/problem/C Tags: binary search, data structures, dp, *1700 Ref: https://codeforces.com/contest/567/submission/164543352

--- codeforces/567/C.cpp
+++ codeforces/567/C.cpp
@@ -13,6 +13,19 @@ const ll INF = 1e18;
 const ll N = 1e5 + 5;
 const ll MOD = 1e9 + 7;  // 998244353
 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// member functions :
+// 1. order_of_key(k) : number of elements strictly lesser than k
+// 2. find_by_order(k) : k-th element in the set
+template <class key, class value, class cmp = std::less<key>>
+using o_map = __gnu_pbds::tree<key, value, cmp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
 void test() {
     ll n, k;
     cin >> n >> k;
@@ -20,14 +33,18 @@ void test() {
     for (ll &A : a) cin >> A;
     if (n < 3) return void(cout << "0\n");
     // lets fix middle element, thus we need count of a[i]/k, before element, and k * a[i] after element
-    unordered_map<ll, ll> pre, suff;
+    o_multiset<ll> pre, suff;
+    auto get = [&](o_multiset<ll> &x, ll element) {
+        return x.order_of_key(element + 1) - x.order_of_key(element);
+    };
     ll ans = 0;
-    for (int i = 1; i < n; ++i) ++suff[a[i]];
+    for (int i = 1; i < n; ++i) suff.insert(a[i]);
     for (int i = 1; i < n; ++i) {
-        ++pre[a[i - 1]];
-        --suff[a[i]];
+        pre.insert(a[i - 1]);
+        auto it = suff.find_by_order(suff.order_of_key(a[i]));
+        suff.erase(it);
         if (a[i] % k == 0)
-            ans += pre[a[i] / k] * suff[a[i] * k];
+            ans += get(pre, a[i] / k) * get(suff, a[i] * k);
     }
     cout << ans << '\n';
 }
