4c014e7cc08d551d8a3a80727708979a97f829a4
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-18 15:15:00 +0530

    Add solution for problem `E - Tetrahedron` Link: https://codeforces.com/contest/166/problem/E Tags: dp, math, matrices, *1500 Ref: https://codeforces.com/contest/166/submission/164704127

--- codeforces/166/E.cpp
+++ codeforces/166/E.cpp
@@ -1,88 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll MOD = 1e9 + 7;
 
-struct Matrix {
-    static const ll M = MOD;
-    static const ll SQMOD = M * M;
-    static ll const N = 10;
-    ll mat[N][N];
-    ll n, m;
-    Matrix(ll _n = N, ll _m = N, ll val = 0) : n(_n), m(_m) {
-        for (ll i = 0; i < n; ++i)
-            for (ll j = 0; j < m; ++j)
-                mat[i][j] = val;
-    }
-    Matrix(const vector<vector<ll>> &&other) {
-        n = ll(other.size());
-        m = ll(other[0].size());
-        for (ll i = 0; i < n; ++i)
-            for (ll j = 0; j < m; ++j)
-                mat[i][j] = other[i][j];
-    }
-    Matrix &operator=(const vector<vector<ll>> &&other) {
-        return *this = Matrix(forward<decltype(other)>(other));
-    }
-    ll *operator[](ll r) { return mat[r]; }
-    const ll *operator[](ll r) const { return mat[r]; }
-    static Matrix unit(ll n) {
-        Matrix res(n, n);
-        for (ll i = 0; i < n; i++) res[i][i] = 1;
-        return res;
-    }
-    Matrix &operator+=(const Matrix &rhs) {
-        assert(n == rhs.n && m == rhs.m);
-        for (ll i = 0; i < n; ++i)
-            for (ll j = 0; j < m; ++j) {
-                mat[i][j] += rhs[i][j];
-                if (mat[i][j] >= M) mat[i][j] -= M;
-            }
-        return *this;
-    }
-    Matrix operator+(const Matrix &rhs) const {
-        Matrix lhs(*this);
-        return lhs += rhs;
-    }
-    friend Matrix operator*(const Matrix &A, const Matrix &B) {
-        assert(A.m == B.n);
-        Matrix res(A.n, B.m);
-        for (ll i = 0; i < res.n; i++)
-            for (ll j = 0; j < res.m; j++) {
-                ll sum = 0LL;
-                for (ll k = 0; k < A.m; k++) {
-                    sum += A[i][k] * B[k][j];
-                    if (sum >= SQMOD) sum -= SQMOD;
-                }
-                res[i][j] = (sum % M);
-            }
-        return res;
-    }
-    friend Matrix power(Matrix base, long long ex) {
-        assert(base.n == base.m);
-        Matrix res = Matrix::unit(base.n);
-        while (ex > 0) {
-            if (ex & 1) res = res * base;
-            base = base * base;
-            ex >>= 1;
-        }
-        return res;
-    }
-    friend string to_string(const Matrix &a) {
-        string res = "\n";
-        for (ll i = 0; i < a.n; ++i) {
-            res += '{';
-            for (ll j = 0; j < a.m; ++j) {
-                res += std::to_string(a.mat[i][j]);
-                if (j != a.m - 1) res += ", ";
-            }
-            res += "}\n";
-        }
-        res.append("\n");
-        return res;
-    }
-    Matrix &operator*=(const Matrix &rhs) { return *this = *this * rhs; }
-};
+const ll N = 1e7 + 5;
+const ll MOD = 1e9 + 7;
+ll dp[2][2];
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
@@ -91,16 +13,29 @@ int main() {
     cin >> n;
     /*
     ? M1
-    Matrix Base = {{{0}, {0}, {0}, {1}}};
-    Matrix Transform = {{{0, 1, 1, 1},
-                         {1, 0, 1, 1},
-                         {1, 1, 0, 1},
-                         {1, 1, 1, 0}}};
-    cout << (power(Transform, n) * Base)[3][0] << '\n';
+    // dp[i][0] = currently at A at ith step
+    // dp[i][1] = currently at B at ith step
+    // dp[i][2] = currently at C at ith step
+    // dp[i][3] = currently at D at ith step
+    for (int i = 0; i < 4; ++i) dp[0][i] = dp[1][i] = 0;
+    dp[0][3] = 1;
+    for (int i = 1; i <= n; ++i) {
+        dp[i % 2][0] = dp[(i + 1) % 2][1] + dp[(i + 1) % 2][2] + dp[(i + 1) % 2][3];
+        dp[i % 2][1] = dp[(i + 1) % 2][0] + dp[(i + 1) % 2][2] + dp[(i + 1) % 2][3];
+        dp[i % 2][2] = dp[(i + 1) % 2][1] + dp[(i + 1) % 2][0] + dp[(i + 1) % 2][3];
+        dp[i % 2][3] = dp[(i + 1) % 2][1] + dp[(i + 1) % 2][2] + dp[(i + 1) % 2][0];
+    }
+    cout << dp[n & 1][3] << '\n';
     */
 
     //? M2
-    Matrix Base = {{{1}, {0}}};
-    Matrix Transform = {{{0, 3}, {1, 2}}};
-    cout << (power(Transform, n) * Base)[0][0] << '\n';
+    // dp[i][0] = ways to be at top of tetrahedron after i moves
+    // dp[i][1] = at one of the lower ones
+    dp[0][0] = 1;
+    dp[0][1] = dp[1][0] = dp[1][1] = 0;
+    for (int i = 1; i <= n; ++i) {
+        dp[i & 1][0] = (3 * dp[(i + 1) & 1][1]) % MOD;
+        dp[i & 1][1] = (2 * dp[(i + 1) & 1][1] + dp[(i + 1) & 1][0]) % MOD;
+    }
+    cout << dp[n & 1][0];
 }
