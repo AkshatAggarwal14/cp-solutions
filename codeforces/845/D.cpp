b60c185437286fc11a57b28c896cc5b546977340
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-21 01:29:00 +0530

    Add solution for problem `D - Driving Test` Link: https://codeforces.com/contest/845/problem/D Tags: data structures, dp, greedy, *1800 Ref: https://codeforces.com/contest/845/submission/165050730

--- codeforces/845/D.cpp
+++ codeforces/845/D.cpp
@@ -27,33 +27,27 @@ void test() {
             case 1:
                 cin >> x;
                 curr = x;
-                // remove prev larger limits
                 while (!limits.empty() && limits.back() < curr)
                     limits.pop_back(), ++violations;
                 break;
             case 2:
-                // just overtook, so remove previous no overtakes
                 violations += no_overtakes;
                 no_overtakes = 0;
                 break;
             case 3:
-                // add to speed limits
                 cin >> x;
                 limits.push_back(x);
                 break;
             case 4:
-                // as can freely overtake now, clear no overtakes
                 no_overtakes = 0;
                 break;
             case 5:
-                // as no limit anymore, remove all previous limits
                 limits.clear();
                 break;
             case 6:
                 ++no_overtakes;
                 break;
         }
-        // new limits added and thus can effect if "curr" speed can be achieved
         while (!limits.empty() && limits.back() < curr)
             limits.pop_back(), ++violations;
     }
