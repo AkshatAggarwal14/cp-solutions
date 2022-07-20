d23ecbabae4cb795232e78ebb2ecbbd0d31fca5f
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2022-07-21 01:33:00 +0530

    Add solution for problem `D - Driving Test` Link: https://codeforces.com/contest/845/problem/D Tags: data structures, dp, greedy, *1800 Ref: https://codeforces.com/contest/845/submission/165051092

--- codeforces/845/D.cpp
+++ codeforces/845/D.cpp
@@ -1,12 +1,21 @@
-#include <bits/stdc++.h>
+#ifdef LOCAL
+#include "Akshat.hpp"
+#else
+#include "bits/stdc++.h"
 using namespace std;
+#define dbg(...)
+#endif
 using ll = long long;
+auto sz = [](const auto &container) { return int(container.size()); };
+#define all(x) begin(x), end(x)
 
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
+const ll INF = 1e18;
+const ll N = 1e5 + 5;
+const ll MOD = 1e9 + 7;  // 998244353
 
+void test() {
     // speed and overtake violations are independent
-    int n;
+    ll n;
     cin >> n;
     int violations = 0;
     int no_overtakes = 0;
@@ -49,4 +58,19 @@ int main() {
             limits.pop_back(), ++violations;
     }
     cout << violations << '\n';
+}
+
+int32_t main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifdef LOCAL
+    [[maybe_unused]] FILE *in = freopen("input.txt", "r", stdin);
+    [[maybe_unused]] FILE *out = freopen("output.txt", "w", stdout);
+#endif
+    cout << fixed << setprecision(12);
+    int tc = 1;
+    // cin >> tc;
+    for (int tt = 1; tt <= tc; ++tt) {
+        test();
+    }
+    return 0;
 }
